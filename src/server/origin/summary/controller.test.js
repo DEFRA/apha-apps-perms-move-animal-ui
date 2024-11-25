dca3cddc3bdd2099cb304cde7fdfba0d3c0b36f7
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2024-11-25 09:32:26 +0000

    added negative tests

--- src/server/origin/summary/controller.test.js
+++ src/server/origin/summary/controller.test.js
@@ -12,7 +12,21 @@ import SesessionTestHelper from '../../common/test-helpers/session-helper.js'
 describe('#originSummaryController', () => {
   /** @type {Server} */
   let server
+
+  /** @type {SesessionTestHelper} */
   let session
+
+  const defaultState = {
+    onOffFarm: 'off',
+    cphNumber: '12/123/1234',
+    address: {
+      addressLine1: 'Starfleet Headquarters',
+      addressLine2: '24-593 Federation Drive',
+      addressTown: 'San Francisco',
+      addressCounty: 'San Francisco',
+      addressPostcode: 'RG24 8RR'
+    }
+  }
 
   beforeAll(async () => {
     server = await createServer()
@@ -22,17 +36,7 @@ describe('#originSummaryController', () => {
   beforeEach(async () => {
     session = await SesessionTestHelper.create(server)
 
-    await session.setState('origin', {
-      onOffFarm: 'off',
-      cphNumber: '12/123/1234',
-      address: {
-        addressLine1: '73 OCEANA CRESCENT',
-        addressLine2: 'Archronos Ltd',
-        addressTown: 'Basingstoke',
-        addressCounty: 'Hampshire',
-        addressPostcode: 'RG224FF'
-      }
-    })
+    await session.setState('origin', defaultState)
   })
 
   afterAll(async () => {
@@ -57,6 +61,109 @@ describe('#originSummaryController', () => {
     expect(statusCode).toBe(statusCodes.ok)
 
     expect(payload).toEqual(expect.stringContaining('12/123/1234'))
+    expect(payload).toEqual(expect.stringContaining('Off the farm or premises'))
+    expect(payload).toEqual(
+      expect.stringContaining(
+        Object.values(defaultState.address).join('<br />')
+      )
+    )
+  })
+
+  describe('should exclude specific fields when missing from state', () => {
+    it('should not show on/off farm text', async () => {
+      await session.setState('origin', {
+        ...defaultState,
+        onOffFarm: undefined
+      })
+
+      const { payload } = await server.inject(
+        withCsrfProtection(
+          {
+            method: 'GET',
+            url: '/origin/summary'
+          },
+          {
+            Cookie: session.sessionID
+          }
+        )
+      )
+
+      expect(payload).not.toEqual(
+        expect.stringContaining('On to the farm or premises')
+      )
+      expect(payload).not.toEqual(
+        expect.stringContaining('Off the farm or premises')
+      )
+
+      expect(payload).toEqual(expect.stringContaining('12/123/1234'))
+      expect(payload).toEqual(
+        expect.stringContaining(
+          Object.values(defaultState.address).join('<br />')
+        )
+      )
+    })
+
+    it('should not show address text', async () => {
+      await session.setState('origin', {
+        ...defaultState,
+        address: undefined
+      })
+
+      const { payload } = await server.inject(
+        withCsrfProtection(
+          {
+            method: 'GET',
+            url: '/origin/summary'
+          },
+          {
+            Cookie: session.sessionID
+          }
+        )
+      )
+
+      expect(payload).toEqual(
+        expect.stringContaining('Off the farm or premises')
+      )
+
+      expect(payload).toEqual(expect.stringContaining('12/123/1234'))
+
+      expect(payload).not.toEqual(
+        expect.stringContaining(
+          Object.values(defaultState.address).join('<br />')
+        )
+      )
+    })
+
+    it('should not show cph number', async () => {
+      await session.setState('origin', {
+        ...defaultState,
+        cphNumber: undefined
+      })
+
+      const { payload } = await server.inject(
+        withCsrfProtection(
+          {
+            method: 'GET',
+            url: '/origin/summary'
+          },
+          {
+            Cookie: session.sessionID
+          }
+        )
+      )
+
+      expect(payload).toEqual(
+        expect.stringContaining('Off the farm or premises')
+      )
+
+      expect(payload).not.toEqual(expect.stringContaining('12/123/1234'))
+
+      expect(payload).toEqual(
+        expect.stringContaining(
+          Object.values(defaultState.address).join('<br />')
+        )
+      )
+    })
   })
 
   testCsrfProtectedGet(() => server, {
