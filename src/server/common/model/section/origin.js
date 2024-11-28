dc7cd44f0f81aaf34246705019dcbbccedbf780b
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2024-11-28 19:36:50 +0000

    Unit tests for origin model

--- src/server/common/model/section/origin.js
+++ src/server/common/model/section/origin.js
@@ -1,19 +1,24 @@
 import Joi from 'joi'
-import { AnswerModel, validateAgainstSchema } from '../answer/answer-model.js'
+import {
+  SectionModel,
+  validateAgainstSchema
+} from '../section/section-model.js'
 import { OnOffFarm } from '../answer/on-off-farm.js'
 import { CphNumber } from '../answer/cph-number.js'
 import { Address } from '../answer/address.js'
 
-const validationSchema = Joi.object().custom((value, helpers) => {
-  const invalid = Object.values(value).some((item) => {
-    return !item.validate().isValid
-  })
+const validationSchema = Joi.object()
+  .custom((origin, helpers) => {
+    const invalid = Object.values(origin).some((item) => {
+      return !item.validate().isValid
+    })
 
-  if (invalid) {
-    return helpers.error('any.invalid')
-  }
-  return value
-})
+    if (invalid) {
+      return helpers.error('any.invalid')
+    }
+    return origin
+  })
+  .label('origin')
 
 /**
  * export @typedef {{
@@ -27,7 +32,7 @@ const validationSchema = Joi.object().custom((value, helpers) => {
  * @import {AddressData} from '../answer/address.js'
  */
 
-export class Origin extends AnswerModel {
+export class Origin extends SectionModel {
   validate() {
     return validateAgainstSchema(validationSchema, this._data)
   }
