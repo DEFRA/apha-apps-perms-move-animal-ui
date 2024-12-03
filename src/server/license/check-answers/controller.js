abacc9bd06d73901375faedadcc5b42ddd812149
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2024-12-03 21:22:48 +0000

    Using current approach until refactor

--- src/server/license/check-answers/controller.js
+++ src/server/license/check-answers/controller.js
@@ -1,3 +1,5 @@
+import { EmailAddress } from '../../common/model/answer/email-address.js'
+
 const indexView = 'license/check-answers/index.njk'
 export const pageTitle =
   'Check your answers before you continue your application'
@@ -9,6 +11,15 @@ export const heading = pageTitle
 export const licenseCheckAnswersGetController = {
   handler(req, h) {
     const license = req.yar.get('license') ?? {}
+
+    const emailAddress = EmailAddress.fromState(
+      license?.emailAddress
+    ).validate()
+    if (!emailAddress.isValid) {
+      return h.redirect(
+        '/receiving-the-licence/licence-enter-email-address?redirect_uri=/receiving-the-licence/check-answers'
+      )
+    }
 
     return h.view(indexView, {
       pageTitle,
