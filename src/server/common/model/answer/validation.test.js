6511d25fe4dfaa7ef0652591a6b1cd8a8f86ad61
Hugh FD Jackson <hugh@hughfdjackson.com>
2024-12-05 13:15:45 +0000

    DSFAAP-520: remove duplicate tests

--- src/server/common/model/answer/validation.test.js
+++ src/server/common/model/answer/validation.test.js
@@ -2,13 +2,9 @@ import Joi from 'joi'
 import { validateAnswerAgainstSchema } from './validation.js'
 
 describe('validateAnswerAgainstSchema', () => {
-  let validationSchema
-
-  beforeEach(() => {
-    validationSchema = Joi.object({
-      addressLine1: Joi.string().required(),
-      postcode: Joi.string().required()
-    })
+  const validationSchema = Joi.object({
+    addressLine1: Joi.string().required(),
+    postcode: Joi.string().required()
   })
 
   it('should return valid result when schema validation passes', () => {
@@ -28,35 +24,5 @@ describe('validateAnswerAgainstSchema', () => {
     expect(result.isValid).toBe(false)
     expect(result.errors).toHaveProperty('postcode')
     expect(result.errors.postcode.text).toMatch(/"postcode" is required/)
-  })
-
-  describe('validateAnswerAgainstSchema', () => {
-    let validationSchema
-
-    beforeEach(() => {
-      validationSchema = Joi.object({
-        addressLine1: Joi.string().required(),
-        postcode: Joi.string().required()
-      })
-    })
-
-    it('should return valid result when schema validation passes', () => {
-      const value = { addressLine1: 'Address line 1', postcode: 'postcode' }
-      const result = validateAnswerAgainstSchema(validationSchema, value)
-
-      expect(result).toEqual({
-        isValid: true,
-        errors: {}
-      })
-    })
-
-    it('should return invalid result with error details when schema validation fails', () => {
-      const value = { addressLine1: 'Address line 1' }
-      const result = validateAnswerAgainstSchema(validationSchema, value)
-
-      expect(result.isValid).toBe(false)
-      expect(result.errors).toHaveProperty('postcode')
-      expect(result.errors.postcode.text).toMatch(/"postcode" is required/)
-    })
   })
 })
