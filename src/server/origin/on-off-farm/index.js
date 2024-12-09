e3e55d28f00eb55b318bbf0b249ce479c0447b32
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2024-12-09 10:16:17 +0000

    uses new models

--- src/server/origin/on-off-farm/index.js
+++ src/server/origin/on-off-farm/index.js
@@ -1,35 +1,46 @@
-import {
-  onOffFarmGetController,
-  onOffFarmPostController
-} from './controller.js'
-
 /**
- * Sets up the routes used in the home page.
+ * Sets up the routes used in the cph number page.
  * These routes are registered in src/server/router.js.
  */
+
+import { Page } from '~/src/server/common/model/page/page-model.js'
+import { PageController } from '~/src/server/common/controller/page-controller.js'
+import { OnOffFarm } from '~/src/server/common/model/answer/on-off-farm.js'
+
+/** @import { AnswerErrors } from "~/src/server/common/model/answer/validation.js" */
+
+export class OnOffFarmPage extends Page {
+  path = '/to-or-from-own-premises'
+  section = 'origin'
+
+  question = 'Are you moving the cattle on or off your farm or premises?'
+
+  questionKey = 'onOffFarm'
+
+  view = 'origin/on-off-farm/index'
+  Answer = OnOffFarm
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  nextPage({ onOffFarm }) {
+    if (onOffFarm === 'on') {
+      return '/exit-page'
+    }
+    return '/origin/cph-number'
+  }
+
+  /** @param {AnswerErrors} errors */
+  errorMessages(errors) {
+    return Object.entries(errors).map(([key, value]) => ({
+      text: value.text,
+      href: `#${key}`
+    }))
+  }
+}
 
 /**
  * @satisfies {ServerRegisterPluginObject<void>}
  */
-export const onOffFarm = {
-  plugin: {
-    name: 'origin-to-or-from-own-premises',
-    register(server) {
-      server.route([
-        {
-          method: 'GET',
-          path: '/to-or-from-own-premises',
-          ...onOffFarmGetController
-        },
-        {
-          method: 'POST',
-          path: '/to-or-from-own-premises',
-          ...onOffFarmPostController
-        }
-      ])
-    }
-  }
-}
+export const onOffFarm = new PageController(new OnOffFarmPage()).plugin()
 
 /**
  * @import { ServerRegisterPluginObject } from '@hapi/hapi'
