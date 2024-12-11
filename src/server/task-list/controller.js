33c86d5972a06924d90c138ed4a9dff7e0095162
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2024-12-11 11:58:42 +0000

    removed uneeded

--- src/server/task-list/controller.js
+++ src/server/task-list/controller.js
@@ -22,14 +22,14 @@ export const taskListGetController = {
 
     const tasks = Object.values(application)
 
-    const gdsTasks = Object.values(application).map((section) => {
-      return buildGdsTaskItem({
+    const gdsTasks = tasks.map((section) => {
+      return {
         title: section.title,
         initialLink: section.initialPage.urlPath,
         summaryLink: section.summaryPageLink,
         isValid: section.validate().isValid,
         isEnabled: section.isEnabled
-      })
+      }
     })
 
     const allTasks = tasks
@@ -44,16 +44,7 @@ export const taskListGetController = {
     return h.view('task-list/index', {
       pageTitle,
       heading,
-      gdsTasks,
-      items: Object.values(application).map((section) => {
-        return {
-          title: section.title,
-          initialLink: section.initialPage.urlPath,
-          summaryLink: section.summaryPageLink,
-          isValid: section.validate().isValid,
-          isEnabled: section.isEnabled
-        }
-      }),
+      tasks: gdsTasks,
       incompleteTasks,
       buttonText
     })
@@ -67,53 +58,6 @@ export const taskListGetController = {
 export const taskListPostController = {
   handler(_req, h) {
     return h.redirect('/submit/check-answers')
-  }
-}
-
-/**
- * @param {object} params - The parameters to configure the GDS task item.
- * @param {string} params.title - The title of the task item.
- * @param {string} params.initialLink - The URL link to navigate when the task is incomplete or not enabled.
- * @param {string} params.summaryLink - The URL link to navigate when the task is completed.
- * @param {boolean} params.isValid - Indicates whether the task is valid or completed.
- * @param {boolean} params.isEnabled - Indicates whether the task is enabled and can be interacted with.
- * @returns {object} A GDS task item object ready to be used in a nunjucks template.
- */
-function buildGdsTaskItem({
-  title,
-  initialLink,
-  summaryLink,
-  isValid,
-  isEnabled
-}) {
-  let status, href
-  if (!isEnabled) {
-    status = {
-      text: 'Cannot start yet',
-      classes: 'govuk-task-list__status--cannot-start-yet'
-    }
-  } else if (isValid) {
-    status = {
-      text: 'Completed',
-      classes: ''
-    }
-    href = summaryLink
-  } else {
-    status = {
-      tag: {
-        text: 'Incomplete',
-        classes: 'govuk-tag--blue'
-      }
-    }
-    href = initialLink
-  }
-
-  return {
-    title: {
-      text: title
-    },
-    href,
-    status
   }
 }
 
