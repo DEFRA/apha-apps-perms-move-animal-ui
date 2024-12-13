2d778f963b5c194cb004ebcda1109dde6f9f93e7
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2024-12-13 15:48:07 +0000

    ewview comments

--- src/server/origin/summary/index.js
+++ src/server/origin/summary/index.js
@@ -1,14 +1,7 @@
-/**
- * Sets up the routes used in the on off farm page.
- * These routes are registered in src/server/router.js.
- */
-
 import SummaryPage from '../../common/model/page/summary-page/SummaryPageModel.js'
 import { SummaryPageController } from '../../common/controller/summary-page-controller/SummaryPageController.js'
 
 import { Origin } from '~/src/server/common/model/section/origin.js'
-
-/** @import { SectionModel } from '~/src/server/common/model/section/section-model/index.js' */
 
 export class OriginSummaryPage extends SummaryPage {
   pageTitle = 'Check your answers before you continue your application'
