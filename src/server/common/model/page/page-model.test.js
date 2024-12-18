f00abade3d9a78c136824593955546c208d849e2
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2024-12-18 12:00:32 +0000

    page model tests

--- src/server/common/model/page/page-model.test.js
+++ src/server/common/model/page/page-model.test.js
@@ -5,4 +5,49 @@ describe('Page', () => {
     const page = new Page()
     expect(page).toHaveProperty('urlPath')
   })
+
+  it('should have a urlKey', () => {
+    const page = new Page()
+    expect(page).toHaveProperty('urlKey')
+  })
+
+  it('should have a sectionKey', () => {
+    const page = new Page()
+    expect(page).toHaveProperty('sectionKey')
+  })
+
+  it('should have a key', () => {
+    const page = new Page()
+    expect(page).toHaveProperty('key')
+  })
+
+  it('should have a view', () => {
+    const page = new Page()
+    expect(page).toHaveProperty('view')
+  })
+
+  it('should have a pageHeading', () => {
+    const page = new Page()
+    expect(page).toHaveProperty('pageHeading')
+  })
+
+  it('should have a pageTitle', () => {
+    const page = new Page()
+    expect(page).toHaveProperty('pageTitle')
+  })
+
+  it('should return pageHeading as heading', () => {
+    const page = new Page()
+    expect(page.heading).toBe(page.pageHeading)
+  })
+
+  it('should return pageTitle as title', () => {
+    const page = new Page()
+    expect(page.title).toBe(page.pageTitle)
+  })
+
+  it('should throw an error on next page by default', () => {
+    const page = new Page()
+    expect(() => page.nextPage()).toThrow()
+  })
 })
