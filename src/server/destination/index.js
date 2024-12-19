b2945fc53296cacc78128ffb05d1817c29776527
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2024-12-19 14:15:08 +0000

    [601] destination summary page

--- src/server/destination/index.js
+++ src/server/destination/index.js
@@ -1,5 +1,6 @@
 import { destinationType } from './destination-type/index.js'
 import { generalLicence } from './general-licence/index.js'
+import { destinationSummary } from './summary/index.js'
 
 /**
  * @satisfies {ServerRegisterPluginObject<void>}
@@ -8,7 +9,11 @@ export const destination = {
   plugin: {
     name: 'destination',
     async register(server) {
-      await server.register([destinationType, generalLicence])
+      await server.register([
+        destinationType,
+        generalLicence,
+        destinationSummary
+      ])
     }
   }
 }
