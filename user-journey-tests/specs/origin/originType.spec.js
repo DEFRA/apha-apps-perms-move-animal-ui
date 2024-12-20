169d256f11b347014070e9c7c491ecc437675d4b
Hugh FD Jackson <hugh@hughfdjackson.com>
2024-12-20 11:23:46 +0000

    DSFAAP-604: add exit page test for origin type

--- user-journey-tests/specs/origin/originType.spec.js
+++ user-journey-tests/specs/origin/originType.spec.js
@@ -3,6 +3,7 @@ import { browser, expect } from '@wdio/globals'
 import originTypePage from '../../page-objects/origin/originTypePage.js'
 import { loadPageAndVerifyTitle } from '../../helpers/page.js'
 import parishHoldingNumberPage from '../../page-objects/origin/parishHoldingNumberPage.js'
+import originTypeExitPage from '../../page-objects/origin/originTypeExitPage.js'
 
 describe('origin type page test', () => {
   beforeEach('Reset browser state and navigate to page', async () => {
@@ -35,7 +36,9 @@ describe('origin type page test', () => {
     await originTypePage.selectAnotherTypeOfPremisesAndContinue()
     await expect(originTypePage.pageError).not.toBeDisplayed()
     await expect(originTypePage.errorSummary).not.toBeDisplayed()
-    // TODO - new page type
+    expect(new URL(await browser.getUrl()).pathname).toBe(
+      `/${originTypeExitPage.pagePath}`
+    )
   })
 
   it('Should choose an option and check its maintained', async () => {
