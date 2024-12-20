d7f022d863cdc66735af2cb57eafa03b30fb6dfd
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2024-12-20 11:27:48 +0000

    Moving api key retrieval to function to test

--- src/server/common/helpers/notify/notify.js
+++ src/server/common/helpers/notify/notify.js
@@ -6,13 +6,13 @@ import { config } from '~/src/config/config.js'
  * @typedef {{ content: string}} NotifyContent
  */
 
-const notifyConfig = config.get('notify')
-const notifyClient = new NotifyClient(notifyConfig.apiKey)
-
 /**
  * @param {NotifyContent} data
  */
 export function sendNotification(data) {
+  const notifyConfig = config.get('notify')
+  const notifyClient = new NotifyClient(notifyConfig.apiKey)
+
   return notifyClient.sendEmail(
     notifyConfig.templateId,
     notifyConfig.caseDeliveryEmailAddress,
