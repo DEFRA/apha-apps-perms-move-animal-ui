7c0dae9478a6636808adce123ecc9d62aee40388
Hugh FD Jackson <hugh@hughfdjackson.com>
2024-12-20 10:31:57 +0000

    DSFAAP-604: add page user journey tests and fix bug

---
+++ user-journey-tests/page-objects/origin/originTypePage.js
@@ -0,0 +1,55 @@
+import { $ } from '@wdio/globals'
+
+import { Page } from '../page.js'
+
+const pageHeadingAndTitle = 'What type of premises are the animals moving off?'
+
+class OriginTypePage extends Page {
+  pagePath = 'origin/type-of-origin'
+  pageHeading = pageHeadingAndTitle
+  pageTitle = pageHeadingAndTitle
+  emptyErrorMessage = 'Select where the animals are moving from'
+
+  get tbRestrictedFarmRadio() {
+    return $('#originType')
+  }
+
+  get approvedFinishingUnitRadio() {
+    return $('#afu-radio')
+  }
+
+  get anotherTypeOfPremisesRadio() {
+    return $('#other-radio')
+  }
+
+  get errorLink() {
+    return $('[href="#originType"]')
+  }
+
+  get pageError() {
+    return $('#originType-error')
+  }
+
+  async selectTBRestrictedFarmAndContinue() {
+    await super.selectRadioAndContinue(this.tbRestrictedFarmRadio)
+  }
+
+  async selectApprovedFinishingUnitAndContinue() {
+    await super.selectRadioAndContinue(this.approvedFinishingUnitRadio)
+  }
+
+  async selectAnotherTypeOfPremisesAndContinue() {
+    await super.selectRadioAndContinue(this.anotherTypeOfPremisesRadio)
+  }
+
+  async originTypeErrorTest() {
+    await super.selectContinue()
+    await super.verifyErrorsOnPage(this.pageError, this.emptyErrorMessage)
+    await super.verifySummaryErrorLink(
+      this.errorLink,
+      this.tbRestrictedFarmRadio
+    )
+  }
+}
+
+export default new OriginTypePage()
