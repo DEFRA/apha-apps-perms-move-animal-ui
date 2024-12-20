d1909fd894f69afaa0d99c748defeb7a2cbbdee9
Hugh FD Jackson <hugh@hughfdjackson.com>
2024-12-20 15:15:23 +0000

    DSFAAP-515: use notify HTTP API (WIP)

--- src/server/common/helpers/notify/notify.js
+++ src/server/common/helpers/notify/notify.js
@@ -1,25 +1,43 @@
-// eslint-disable-next-line n/no-unpublished-import
-import { NotifyClient } from 'notifications-node-client'
 import { config } from '~/src/config/config.js'
+import { token } from '@hapi/jwt'
+import { proxyFetch } from '../proxy.js'
 
 /**
  * @typedef {{ content: string}} NotifyContent
  */
-
 const notifyConfig = config.get('notify')
+
+const secrets = (apiKey) => ({
+  iss: apiKey.substring(apiKey.length - 36, apiKey.length),
+  secret: apiKey.substring(apiKey.length - 73, apiKey.length - 37)
+})
+
+function createToken({ iss, secret }) {
+  const iat = Math.round(Date.now() / 1000)
+
+  return token.generate({ iss, iat }, secret, {
+    header: { typ: 'JWT', alg: 'HS256' }
+  })
+}
 
 /**
  * @param {NotifyContent} data
  */
 export function sendNotification(data) {
-  const notifyClient = new NotifyClient(notifyConfig.apiKey)
+  const body = JSON.stringify({
+    template_id: notifyConfig.templateId,
+    email_address: notifyConfig.caseDeliveryEmailAddress,
+    personalisation: data
+  })
 
-  return notifyClient.sendEmail(
-    notifyConfig.templateId,
-    notifyConfig.caseDeliveryEmailAddress,
+  return proxyFetch(
+    'https://api.notifications.service.gov.uk/v2/notifications/email',
     {
-      personalisation: data,
-      reference: null
+      method: 'POST',
+      body,
+      headers: {
+        Authorization: 'Bearer ' + createToken(secrets(notifyConfig.apiKey))
+      }
     }
   )
 }
