4ea0017f51527503c040eab796eb0e8f4173548b
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-01-02 13:25:45 +0000

    update tests

--- src/server/destination/another-destination/index.test.js
+++ src/server/destination/another-destination/index.test.js
@@ -1,23 +1,12 @@
 import { AnotherDestinationExitPage } from './index.js'
 
 describe('#AnotherDestinationController', () => {
-  let page
-
-  beforeEach(() => {
-    page = new AnotherDestinationExitPage()
-  })
-
-  it('should have the correct title', () => {
+  it('should have the correct properties', () => {
+    const page = new AnotherDestinationExitPage()
     expect(page.pageTitle).toBe(
       'This service is not available for your movement type'
     )
-  })
-
-  it('should have the correct path', () => {
     expect(page.urlPath).toBe('/destination/can-not-use-service')
-  })
-
-  it('should have the correct view', () => {
     expect(page.view).toBe('destination/another-destination/index')
   })
 })
