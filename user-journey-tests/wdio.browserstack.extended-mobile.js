60bbaf18bedba84d86f0a80ff536b6858cd03240
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-01-06 09:14:27 +0000

    BAU: move iOS tests into their own run
    We're seeing multiple intermittent in our browserstack extended tests,
    but they only appear to be within iOS.
    
    This creates a 'noisy alert' problem - if we expect our extended tests
    to failure frequently, we will start taking test failure less seriously
    (since it's likely just the known iOS issue).  Which, in turn, makes it
    likely that we'll respond slower to regressions on the stable test run
    targets - the desktop browsers.
    
    Separating out the unstable mobile tests from the desktop tests gives us
    a functioning test signal again for the desktop arena.

--- user-journey-tests/wdio.browserstack.extended-mobile.js
+++ user-journey-tests/wdio.browserstack.extended-mobile.js
@@ -3,44 +3,8 @@ import { config as browserstackBaseConfig } from './wdio.browserstack.conf.js'
 //  These capabilities should run as an extended test after publishing
 export const config = {
   ...browserstackBaseConfig,
-  maxInstances: 7,
+  maxInstances: 3,
   capabilities: [
-    {
-      browserName: 'Chrome',
-      'bstack:options': {
-        browserVersion: 'latest',
-        os: 'Windows',
-        osVersion: '10',
-        sessionName: 'windows-chrome'
-      }
-    },
-    {
-      browserName: 'Firefox',
-      'bstack:options': {
-        browserVersion: 'latest',
-        os: 'Windows',
-        osVersion: '10',
-        sessionName: 'windows-firefox'
-      }
-    },
-    {
-      browserName: 'Chrome',
-      'bstack:options': {
-        browserVersion: 'latest',
-        os: 'OS X',
-        osVersion: 'Monterey',
-        sessionName: 'osx-chrome'
-      }
-    },
-    {
-      browserName: 'Firefox',
-      'bstack:options': {
-        browserVersion: 'latest',
-        os: 'OS X',
-        osVersion: 'Monterey',
-        sessionName: 'osx-firefox'
-      }
-    },
     {
       browserName: 'Safari',
       'bstack:options': {
