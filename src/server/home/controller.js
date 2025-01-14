26dbda37707c26433e9316686633b2aa0089651b
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-01-14 16:19:01 +0000

    use service name on start page

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -1,3 +1,5 @@
+import { config } from '~/src/config/config.js'
+
 /**
  * A GDS styled example home page controller.
  * Provided as an example, remove or modify as required.
@@ -6,7 +8,7 @@
 export const homeController = {
   handler(_request, h) {
     return h.view('home/index', {
-      pageTitle: 'Apply for a Bovine Tuberculosis (TB) movement licence',
+      pageTitle: config.get('serviceName'),
       heading: 'Apply for a Bovine Tuberculosis (TB) movement licence'
     })
   }
