98c42363f00a2b53aebf62efc7f86b4c0a0039da
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-01-16 07:43:59 +0000

    BAU: remove unused exit page definition

--- src/server/destination/exit-page/index.js
+++
@@ -1,6 +0,0 @@
-import { ExitPage } from '../../common/model/page/exit-page-model.js'
-
-export class DestinationExitPage extends ExitPage {
-  urlPath = '/destination/can-not-use-service'
-}
-export const destinationExitPage = new DestinationExitPage()
