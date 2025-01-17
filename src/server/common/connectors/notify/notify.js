2e3d52e28be3dabdddb9860e60b25cdf97a85abc
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-01-17 13:37:26 +0000

    BAU: refactor to async function

--- src/server/common/connectors/notify/notify.js
+++ src/server/common/connectors/notify/notify.js
@@ -10,14 +10,14 @@ const notifyConfig = config.get('notify')
 /**
  * @param {NotifyContent} data
  */
-export function sendNotification(data) {
+export async function sendNotification(data) {
   const body = JSON.stringify({
     template_id: notifyConfig.templateId,
     email_address: notifyConfig.caseDeliveryEmailAddress,
     personalisation: data
   })
 
-  return proxyFetch(
+  const response = await proxyFetch(
     'https://api.notifications.service.gov.uk/v2/notifications/email',
     {
       method: 'POST',
@@ -26,14 +26,14 @@ export function sendNotification(data) {
         Authorization: 'Bearer ' + createToken(notifyConfig.apiKey)
       }
     }
-  ).then(async (response) => {
-    if (!response.ok) {
-      const responseBody = await response.json()
-      const errors = responseBody.errors.map((error) => error.message)
-      throw new Error(
-        `HTTP failure from GOV.uk notify: status ${response.status} with the following errors: ${errors.join(', ')}`
-      )
-    }
-    return response
-  })
+  )
+
+  if (!response.ok) {
+    const responseBody = await response.json()
+    const errors = responseBody.errors.map((error) => error.message)
+    throw new Error(
+      `HTTP failure from GOV.uk notify: status ${response.status} with the following errors: ${errors.join(', ')}`
+    )
+  }
+  return response
 }
