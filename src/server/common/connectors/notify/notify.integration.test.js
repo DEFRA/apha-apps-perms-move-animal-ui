bfff44207cffed53d343c3700f5d8e18f21bee54
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-01-23 06:42:20 +0000

    DSFAAP-747: avoid mutating config object

--- src/server/common/connectors/notify/notify.integration.test.js
+++ src/server/common/connectors/notify/notify.integration.test.js
@@ -3,11 +3,19 @@ import { config } from '~/src/config/config.js'
 
 describe('sendNotification (integration)', () => {
   it('should abort if the configured timeout is hit', async () => {
-    const notifyConfig = config.get('notify')
-    config.set('notify', {
-      ...notifyConfig,
+    const configGet = config.get.bind(config)
+    const notifyConfig = {
+      ...config.get('notify'),
       timeout: 0
+    }
+    jest.spyOn(config, 'get').mockImplementation((name) => {
+      if (name === 'notify') {
+        return notifyConfig
+      } else {
+        return configGet(name)
+      }
     })
+
     const testData = { content: 'test' }
 
     const result = sendNotification(testData)
