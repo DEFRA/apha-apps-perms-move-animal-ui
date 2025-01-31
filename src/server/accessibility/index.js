504de8da0aa289a90c6f77b8a6247c3de1293a6e
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-01-31 15:14:45 +0000

    Moved page title to const

--- src/server/accessibility/index.js
+++ src/server/accessibility/index.js
@@ -1,11 +1,13 @@
 import { Page } from '~/src/server/common/model/page/page-model.js'
 import { PageController } from '~/src/server/common/controller/page-controller/page-controller.js'
 
+const pageTitle = 'Accessibility statement'
+
 export class AccessibilityStatementPage extends Page {
   sectionKey = 'policies'
   key = 'accessibility-statement'
-  pageTitle = 'Accessibility statement'
-  pageHeading = 'Accessibility statement'
+  pageTitle = pageTitle
+  pageHeading = pageTitle
   urlPath = '/accessibility-statement'
   view = 'accessibility/index'
 }
