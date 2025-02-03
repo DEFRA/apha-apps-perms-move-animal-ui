49c8aa777ebaa12f604120663c442452d85b6b39
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-03 08:43:41 +0000

    DSFAAP-799: simplify cph number test
    Now the logic resides in the text input, these tests can be largely
    configuration based.

--- src/server/common/model/answer/cph-number/cph-number.test.js
+++ src/server/common/model/answer/cph-number/cph-number.test.js
@@ -1,99 +1,72 @@
+import { TextAnswer } from '../text/text.js'
 import { CphNumberAnswer } from './cph-number.js'
 
-const validCphNumberPayload = {
+const validPayload = {
   cphNumber: '12/456/7899'
 }
 
-describe('CphNumber.new', () => {
+describe('CphNumberAnswer', () => {
+  it('should be a text input', () => {
+    expect(new CphNumberAnswer(validPayload)).toBeInstanceOf(TextAnswer)
+  })
+
+  it('should have the right payload key', () => {
+    expect(CphNumberAnswer.config.payloadKey).toBe('cphNumber')
+  })
+
+  it('should autocomplete', () => {
+    expect(CphNumberAnswer.config.autocomplete).toBe('cph-number')
+  })
+
+  it('should strip whitespace', () => {
+    expect(CphNumberAnswer.config.stripWhitespace).toBe(true)
+  })
+
+  it('should display with a char width of 10', () => {
+    expect(CphNumberAnswer.config.characterWidth).toBe(10)
+  })
+
+  it('should have a hint', () => {
+    expect(CphNumberAnswer.config.hint).toBe('For example, 12/345/6789')
+  })
+
   it('should strip away any irrelevant values', () => {
-    const payload = { ...validCphNumberPayload, nextPage: '/other/page' }
+    const payload = { ...validPayload, nextPage: '/other/page' }
     const cphNumber = new CphNumberAnswer(payload)
 
-    expect(cphNumber._data).toEqual(validCphNumberPayload)
-  })
-})
-
-describe('#CphNumber.validate', () => {
-  it('should return true for valid cphNumber', () => {
-    const cphNumber = new CphNumberAnswer(validCphNumberPayload)
-    const { isValid, errors } = cphNumber.validate()
-
-    expect(isValid).toBe(true)
-    expect(errors).toEqual({})
+    expect(cphNumber._data).toEqual(validPayload)
   })
 
-  it('should return false for an empty input', () => {
-    const cphNumber = new CphNumberAnswer()
-    const { isValid, errors } = cphNumber.validate()
+  describe('#CphNumberAnswer.validate', () => {
+    it('should return true for valid cphNumber', () => {
+      const cphNumber = new CphNumberAnswer(validPayload)
+      const { isValid, errors } = cphNumber.validate()
 
-    expect(isValid).toBe(false)
-    expect(errors.cphNumber.text).toBe('Enter the farm or premises CPH number')
-  })
-
-  it('should return false for malformed input', () => {
-    const cphNumber = new CphNumberAnswer({
-      cphNumber: '1/2/3'
+      expect(isValid).toBe(true)
+      expect(errors).toEqual({})
     })
 
-    const { isValid, errors } = cphNumber.validate()
+    it('should return false for an empty input', () => {
+      const cphNumber = new CphNumberAnswer()
+      const { isValid, errors } = cphNumber.validate()
 
-    expect(isValid).toBe(false)
-    expect(errors.cphNumber.text).toBe(
-      'Enter the CPH number in the correct format, for example, 12/345/6789'
-    )
-  })
-})
-
-describe('#CphNumber.toState', () => {
-  it('should replace missing data with blank string', () => {
-    const cphNumber = new CphNumberAnswer()
-    const data = cphNumber.toState()
-
-    expect(data).toBe('')
-  })
-
-  it('should pass through valid data unaltered', () => {
-    const cphNumber = new CphNumberAnswer(validCphNumberPayload)
-    const data = cphNumber.toState()
-
-    expect(data).toEqual(validCphNumberPayload.cphNumber)
-  })
-
-  it('should remove whitespace', () => {
-    const cphNumber = new CphNumberAnswer({
-      cphNumber: '  12/4 56/789 9 '
+      expect(isValid).toBe(false)
+      expect(errors.cphNumber.text).toBe(
+        'Enter the farm or premises CPH number'
+      )
     })
 
-    expect(cphNumber.toState()).toBe('12/456/7899')
-  })
-})
+    it('should return false for malformed input', () => {
+      const cphNumber = new CphNumberAnswer({
+        cphNumber: '1/2/3'
+      })
 
-describe('#CphNumber.fromState', () => {
-  it('should return just the cphNumber from the payload', () => {
-    const cphNumber = new CphNumberAnswer(validCphNumberPayload)
-    const state = cphNumber.toState()
-    expect(CphNumberAnswer.fromState(state).value).toEqual(
-      validCphNumberPayload.cphNumber
-    )
-  })
+      const { isValid, errors } = cphNumber.validate()
 
-  it('should return an undefined value if the state is undefined', () => {
-    expect(CphNumberAnswer.fromState(undefined).value).toBeUndefined()
-  })
-
-  it('should return an empty object if the state is undefined', () => {
-    expect(CphNumberAnswer.fromState(undefined)._data).toEqual({})
-  })
-})
-
-describe('#CphNumber.html', () => {
-  it('should return the cphNumber if present', () => {
-    const cphNumber = new CphNumberAnswer(validCphNumberPayload)
-    expect(cphNumber.html).toBe(validCphNumberPayload.cphNumber)
-  })
-
-  it('should return an empty string if cphNumber is not present', () => {
-    const cphNumber = new CphNumberAnswer()
-    expect(cphNumber.html).toBe('')
+      expect(isValid).toBe(false)
+      expect(errors.cphNumber.text).toBe(
+        'Enter the CPH number in the correct format, for example, 12/345/6789'
+      )
+    })
   })
 })
