16a8a53492a76aab58f90ba69f92db44911e715f
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-03 08:19:21 +0000

    DSFAAP-799: inline config

--- src/server/common/model/answer/origin-type/origin-type.js
+++ src/server/common/model/answer/origin-type/origin-type.js
@@ -1,22 +1,6 @@
 import { RadioButtonAnswer } from '../radio-button/radio-button.js'
 /** @import {RadioButtonConfig} from '../radio-button/radio-button.js' */
 
-/** @type {RadioButtonConfig} */
-const config = {
-  payloadKey: 'originType',
-  options: {
-    'tb-restricted-farm': { label: 'TB restricted farm' },
-    afu: {
-      label: 'Approved finishing unit (AFU)',
-      hint: 'Including enhanced with grazing (AFUE)'
-    },
-    other: { label: 'Another type of premises' }
-  },
-  errors: {
-    emptyOptionText: 'Select where the animals are moving from'
-  }
-}
-
 /**
  * export @typedef {'tb-restricted-farm' | 'afu' | 'other'} OriginTypeData
  * @typedef {{ originType: OriginTypeData }} OriginTypePayload
@@ -24,5 +8,19 @@ const config = {
 
 /** @augments {RadioButtonAnswer<OriginTypePayload>} */
 export class OriginTypeAnswer extends RadioButtonAnswer {
-  static config = config
+  /** @type {RadioButtonConfig} */
+  static config = {
+    payloadKey: 'originType',
+    options: {
+      'tb-restricted-farm': { label: 'TB restricted farm' },
+      afu: {
+        label: 'Approved finishing unit (AFU)',
+        hint: 'Including enhanced with grazing (AFUE)'
+      },
+      other: { label: 'Another type of premises' }
+    },
+    errors: {
+      emptyOptionText: 'Select where the animals are moving from'
+    }
+  }
 }
--- src/server/common/model/answer/receiveMethod/receiveMethod.js
+++ src/server/common/model/answer/receiveMethod/receiveMethod.js
@@ -1,18 +1,6 @@
 import { RadioButtonAnswer } from '../radio-button/radio-button.js'
 /** @import {RadioButtonConfig} from '../radio-button/radio-button.js' */
 
-/** @type {RadioButtonConfig} */
-const config = {
-  payloadKey: 'receiveMethod',
-  options: {
-    email: { label: 'Email' },
-    post: { label: 'Post' }
-  },
-  errors: {
-    emptyOptionText: 'Select how you would like this licence sent to you'
-  }
-}
-
 /**
  * export @typedef {string} ReceiveMethodData
  * @typedef {{ receiveMethod: string }} ReceiveMethodPayload
@@ -20,5 +8,15 @@ const config = {
 
 /** @augments {RadioButtonAnswer<ReceiveMethodPayload>} */
 export class ReceiveMethodAnswer extends RadioButtonAnswer {
-  static config = config
+  /** @type {RadioButtonConfig} */
+  static config = {
+    payloadKey: 'receiveMethod',
+    options: {
+      email: { label: 'Email' },
+      post: { label: 'Post' }
+    },
+    errors: {
+      emptyOptionText: 'Select how you would like this licence sent to you'
+    }
+  }
 }
