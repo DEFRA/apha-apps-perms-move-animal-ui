99d9bf7bbefae38a1726409022e307922ffc9f18
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-02-04 11:53:52 +0000

    starting of tests

---
+++ src/server/biosecurity-map/upload-plan/index.test.js
@@ -0,0 +1,45 @@
+import { createServer } from '~/src/server/index.js'
+import { describePageSnapshot } from '../../common/test-helpers/snapshot-page.js'
+
+/**
+ * @import { IncomingMessage } from 'node:http'
+ * @import { RequestOptions } from '~/src/server/services/httpService.js'
+ */
+
+import Wreck from '@hapi/wreck'
+
+jest.spyOn(Wreck, 'post').mockResolvedValue({
+  res: /** @type {IncomingMessage} */ ({
+    statusCode: 200
+  }),
+  payload: JSON.stringify({
+    uploadId: 'b18ceadb-afb1-4955-a70b-256bf94444d5',
+    uploadUrl: '/upload-and-scan/b18ceadb-afb1-4955-a70b-256bf94444d5',
+    statusUrl:
+      'https://cdp-uploader/status/b18ceadb-afb1-4955-a70b-256bf94444d5'
+  })
+})
+
+describe('#UploadPlan', () => {
+  describePageSnapshot({
+    describes: '#UploadPlanPage',
+    it: 'should render expected response and content',
+    pageUrl: '/biosecurity-map/upload-plan'
+  })
+
+  let server
+
+  beforeAll(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
+
+  it('should render expected response and content', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/biosecurity-map/upload-plan'
+    })
+
+    expect(response.statusCode).toBe(200)
+  })
+})
