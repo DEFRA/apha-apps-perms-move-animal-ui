21c5f64f6dca8560efc3a7551602e73f9a84f074
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-02-05 10:26:38 +0000

    Unit tests

--- src/server/common/model/application/application.test.js
+++ src/server/common/model/application/application.test.js
@@ -2,6 +2,11 @@ import { ApplicationModel } from './application.js'
 import { LicenceSection } from '../section/licence/licence.js'
 import { OriginSection } from '../section/origin/origin.js'
 import { DestinationSection } from '../section/destination/destination.js'
+import { BiosecuritySection } from '../section/biosecurity/biosecurity.js'
+
+/**
+ * @import { Request } from '@hapi/hapi'
+ */
 
 /** @import {OnOffFarmData} from '../answer/on-off-farm/on-off-farm.js' */
 const originDefaultState = {
@@ -25,20 +30,33 @@ const destinationDefaultState = {
   destinationType: 'dedicated-sale'
 }
 
+const biosecurityDefaultState = {
+  keptSeparately: 'yes'
+}
+
 describe('Application', () => {
   it('should create an Application instance from a valid state', () => {
     const state = {
       origin: originDefaultState,
       licence: licenceDefaultState,
-      destination: destinationDefaultState
+      destination: destinationDefaultState,
+      biosecurity: biosecurityDefaultState
     }
 
-    const application = ApplicationModel.fromState(state)
+    const request = /** @type {Request} */ {
+      ...jest.requireActual('@hapi/hapi'),
+      yar: {
+        get: jest.fn().mockImplementation((key) => state[key])
+      }
+    }
+
+    const application = ApplicationModel.fromState(request)
 
     expect(application).toBeInstanceOf(ApplicationModel)
 
     expect(application.tasks.origin).toBeInstanceOf(OriginSection)
     expect(application.tasks.licence).toBeInstanceOf(LicenceSection)
     expect(application.tasks.destination).toBeInstanceOf(DestinationSection)
+    expect(application.tasks.biosecurity).toBeInstanceOf(BiosecuritySection)
   })
 })
