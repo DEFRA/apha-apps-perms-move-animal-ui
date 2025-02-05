d8f709a0b421d163420c25cd67c03fa28cb0b606
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-02-05 16:32:42 +0000

    some logging

--- user-journey-tests/specs/biosecurity-map/biosecurityMapUpload.spec.js
+++ user-journey-tests/specs/biosecurity-map/biosecurityMapUpload.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { waitForPagePath } from '../../helpers/page.js'
 import mapUploadPage from '../../page-objects/biosecurity-map/mapUploadPage.js'
 import uploadLoadingPage from '../../page-objects/biosecurity-map/uploadLoadingPage.js'
@@ -5,6 +6,20 @@ import uploadLoadingPage from '../../page-objects/biosecurity-map/uploadLoadingP
 describe('Biosecurity map upload test', () => {
   beforeEach('Navigate to biosecurity map upload page', async () => {
     await mapUploadPage.navigateToPageAndVerifyTitle()
+
+    // Fetch and log the page response
+    const response = await browser.execute(async () => {
+      return fetch(window.location.href, { method: 'GET' })
+        .then((res) => ({
+          url: res.url,
+          status: res.status,
+          statusText: res.statusText,
+          headers: Object.fromEntries(res.headers.entries())
+        }))
+        .catch((err) => ({ error: err.message }))
+    })
+
+    await console.log('🌐 Page Response:', response)
   })
 
   it('Should verify upload flow', async () => {
