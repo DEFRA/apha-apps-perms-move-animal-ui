08ee9f188958017dd26e7cd13be67c8e8c195efc
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-02-06 14:54:44 +0000

    validation works

--- src/server/common/model/answer/biosecurity-map/validation.js
+++ src/server/common/model/answer/biosecurity-map/validation.js
@@ -5,5 +5,14 @@ export default Joi.object({
     uploadId: Joi.string().required(),
     uploadUrl: Joi.string().required(),
     statusUrl: Joi.string().required()
-  })
+  }).required(),
+  status: Joi.object({
+    uploadStatus: Joi.string().required(),
+    metadata: Joi.object().required(),
+    form: Joi.object({
+      crumb: Joi.string().required(),
+      file: Joi.object().required()
+    }),
+    numberOfRejectedFiles: Joi.number().required()
+  }).optional()
 })
