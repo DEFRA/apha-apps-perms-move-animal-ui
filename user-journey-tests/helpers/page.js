2d00d017bbf5b8f092ddc6cdc1aa10516906ad5c
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-06 16:33:02 +0000

    BAU: re-fetch title when waiting for page load

--- user-journey-tests/helpers/page.js
+++ user-journey-tests/helpers/page.js
@@ -54,9 +54,9 @@ export const selectElement = async (element, hidden = false) => {
 }
 
 export const verifyPageTitle = async (pageTitle) => {
-  const receeivedTitle = await browser.getTitle()
-  await browser.waitUntil(async () => receeivedTitle === pageTitle, {
-    timeoutMsg: `Expected page title to become ${pageTitle}\nRecieved:\t${receeivedTitle}\nExpected:\t${pageTitle}`
+  const titleMatches = async () => (await browser.getTitle()) === pageTitle
+  await browser.waitUntil(titleMatches, {
+    timeoutMsg: `Expected page title to become ${pageTitle}\nRecieved:\t${await browser.getTitle()}\nExpected:\t${pageTitle}`
   })
 }
 
