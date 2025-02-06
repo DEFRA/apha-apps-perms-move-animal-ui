7c6f4994e947f43375c1e2b88c74ce59a62a7021
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-02-06 08:16:06 +0000

    added some info to error messages

--- user-journey-tests/helpers/page.js
+++ user-journey-tests/helpers/page.js
@@ -57,7 +57,7 @@ export const verifyPageTitle = async (pageTitle) => {
   await browser.waitUntil(
     async () => (await browser.getTitle()) === pageTitle,
     {
-      timeoutMsg: `Expected page title to become ${pageTitle}`
+      timeoutMsg: `Expected page title to become ${pageTitle}\nRecieved:\t${await browser.getTitle()}\nExpected:\t${pageTitle}`
     }
   )
 }
