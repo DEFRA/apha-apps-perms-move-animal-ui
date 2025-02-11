8a2a99ee5613152ba3b0345551f5e9f8fcc6f43e
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-11 13:39:24 +0000

    DSFAAP-811: answer model accepts application state as context

--- src/server/common/model/answer/answer-model.test.js
+++ src/server/common/model/answer/answer-model.test.js
@@ -1,9 +1,24 @@
 import { NotImplementedError } from '../../helpers/not-implemented-error.js'
 import { AnswerModel } from './answer-model.js'
 
+class AnswerModelBasic extends AnswerModel {
+  _extractFields(data) {
+    return data
+  }
+}
+
 describe('AnswerModel', () => {
   const notImplementedError = 'Not Implemented'
   const answer = new AnswerModel()
+
+  it('should store the data & context passed to it', () => {
+    const appState = { origin: { onOffFarm: 'on' } }
+    const answerPayload = { originType: 'afu' }
+    const answer = new AnswerModelBasic(answerPayload, appState)
+
+    expect(answer._data).toEqual(answerPayload)
+    expect(answer._context).toEqual(appState)
+  })
 
   it('should throw NotImplementedError when value getter is called', () => {
     expect(() => answer.value).toThrow(notImplementedError)
@@ -36,11 +51,6 @@ describe('AnswerModel', () => {
   })
 
   it('should seal the object to prevent property additions or deletions', () => {
-    class AnswerModelBasic extends AnswerModel {
-      _extractFields(data) {
-        return data
-      }
-    }
     const answer = /** @type {any} */ (new AnswerModelBasic({ key: 'value' }))
 
     expect(() => {
