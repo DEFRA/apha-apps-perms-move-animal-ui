59f95dbb2b036d1b22afcbb5b62c54d0a2b1641a
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-20 13:17:34 +0000

    DSFAAP-857: add user journey tests for origin country page

---
+++ user-journey-tests/specs/origin/originCountry.spec.js
@@ -0,0 +1,28 @@
+import { browser } from '@wdio/globals'
+
+import originCountryPage from '../../page-objects/origin/originCountryPage.js'
+
+describe('Country of import test', () => {
+  beforeEach('Reset browser state and navigate to page', async () => {
+    await browser.reloadSession()
+    await originCountryPage.navigateToPageAndVerifyTitle()
+  })
+
+  it('Should verify that page errors when nothing is entered', async () => {
+    await originCountryPage.inputErrorTest('', originCountryPage.noInputError)
+  })
+
+  it('Should verify that page errors when too much text is entered is entered', async () => {
+    const longString = new Array(256).fill('a').join('')
+    await originCountryPage.inputErrorTest(
+      longString,
+      originCountryPage.maxCharacterError
+    )
+  })
+
+  it('Should input country and continue without producing an error', async () => {
+    await originCountryPage.inputTextAndContinue('France')
+    await expect(originCountryPage.fieldError()).not.toBeDisplayed()
+    await expect(originCountryPage.errorSummary).not.toBeDisplayed()
+  })
+})
