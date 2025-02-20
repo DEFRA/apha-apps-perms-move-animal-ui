1cf659087c6e7b4fb6cbbfdcce04e48b05b172aa
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-02-20 14:55:51 +0000

    Parish holding and new check answers validation

--- user-journey-tests/page-objects/origin/onFarmCPHPage.js
+++ user-journey-tests/page-objects/origin/onFarmCPHPage.js
@@ -1,7 +1,12 @@
 import { Page } from '../page.js'
 
+const pageHeadingAndTitle =
+  'What is the County Parish Holding (CPH) number of the farm or premises where the animals are moving off?'
+
 class OnFarmCPHPage extends Page {
   pagePath = 'origin/origin-farm-cph'
+  pageTitle = pageHeadingAndTitle
+  pageHeading = pageHeadingAndTitle
 }
 
 export default new OnFarmCPHPage()
---
+++ user-journey-tests/specs/origin/onFarmParish.spec.js
@@ -0,0 +1,17 @@
+import ParishHoldingNumberPage from '../../page-objects/origin/parishHoldingNumberPage.js'
+import onFarmCPHPage from '../../page-objects/origin/onFarmCPHPage.js'
+import onFarmAddressPage from '../../page-objects/origin/onFarmAddressPage.js'
+
+describe('Paris holding page test', () => {
+  beforeEach('Reset browser state and navigate to page', async () => {
+    await onFarmCPHPage.navigateToPageAndVerifyTitle()
+  })
+
+  it('Should choose an option and check its maintained', async () => {
+    const validInput = '54/321/1234'
+    await ParishHoldingNumberPage.inputParishHoldingNumberAndContinue(
+      validInput,
+      onFarmAddressPage
+    )
+  })
+})
