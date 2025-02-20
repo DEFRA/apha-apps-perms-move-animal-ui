15c23f54fd4526c3427327a92b384dfd68391310
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-20 11:13:08 +0000

    DSFAAP-855: add user journey tests for fifty percent page

---
+++ user-journey-tests/specs/origin/fiftyPercentWarning.spec.js
@@ -0,0 +1,18 @@
+import { waitForExpectedRedirectUri } from '../../helpers/page.js'
+import originCheckAnswersPage from '../../page-objects/origin/checkAnswersPage.js'
+import fiftyPercentWarningPage from '../../page-objects/origin/fiftyPercentWarningPage.js'
+
+describe('General licence page test', () => {
+  beforeEach('Reset browser state and navigate to page', async () => {
+    await fiftyPercentWarningPage.navigateToPageAndVerifyTitle()
+  })
+
+  it('Should verify that clicking the GOV.UK Bovine TB guidance page results in navigating to the selected guidance', async () => {
+    await fiftyPercentWarningPage.verifyGetYourCattleTestedLink()
+  })
+
+  it('Should verify pressing continue gives takes you to the check answers page (which will redirect you to first unanswered question)', async () => {
+    await fiftyPercentWarningPage.selectContinue()
+    await waitForExpectedRedirectUri(originCheckAnswersPage.pagePath)
+  })
+})
