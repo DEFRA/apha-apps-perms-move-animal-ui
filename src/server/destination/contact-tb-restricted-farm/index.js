9bfded496d05c4d2129efca48351316e6bf5f816
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-20 04:52:13 +0000

    DSFAAP-865: add destination exit page when moving on to tb restricted farm

---
+++ src/server/destination/contact-tb-restricted-farm/index.js
@@ -0,0 +1,27 @@
+import { ExitPage } from '../../common/model/page/exit-page-model.js'
+import { PageController } from '../../common/controller/page-controller/page-controller.js'
+
+/**
+ * @import { ServerRegisterPluginObject } from '@hapi/hapi'
+ */
+
+export class ContactTbRestrictedFarmPage extends ExitPage {
+  urlPath = '/destination/contact-the-tb-restricted-farm'
+  pageTitle =
+    'You need to contact the TB restricted farm the animals are moving onto'
+
+  view = `destination/contact-tb-restricted-farm/index`
+  sectionKey = 'destination'
+  key = 'ContactTbRestrictedFarmPage'
+}
+export const contactTbRestrictedFarmPage = new ContactTbRestrictedFarmPage()
+
+/**
+ * @satisfies {ServerRegisterPluginObject<void>}
+ */
+export const contactTbRestrictedFarm = new PageController(
+  contactTbRestrictedFarmPage,
+  {
+    methods: ['GET']
+  }
+).plugin()
