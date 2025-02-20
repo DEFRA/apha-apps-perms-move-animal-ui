a6561d06917c56a2ed6669b8b34999ed2cd6ba5b
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-20 21:09:06 +0000

    DSFAAP-868: add import CPH page

---
+++ src/server/origin/import-address/index.js
@@ -0,0 +1,8 @@
+import { Page } from '../../common/model/page/page-model.js'
+
+export class ImportAddressPage extends Page {
+  urlPath = '/origin/import-address'
+  key = 'ImportCphPage'
+}
+
+export const importAddressPage = new ImportAddressPage()
