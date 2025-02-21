bfdf8c1670a6a676df2e1f957b25987a348a5345
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-02-21 14:20:38 +0000

    update snapshots

--- src/server/common/controller/page-controller/page-controller.js
+++ src/server/common/controller/page-controller/page-controller.js
@@ -56,6 +56,10 @@ export class PageController extends GenericPageController {
   }
 
   handleGet(req, h, opts = {}) {
+    if (this.page instanceof ExitPage) {
+      h.headers.set('Exit-Page', this.page.urlPath)
+    }
+
     return h.view(this.page.view, {
       nextPage: req.query.redirect_uri,
       pageTitle: this.page.title,
--- src/server/home/controller.test.js
+++ src/server/home/controller.test.js
@@ -2,13 +2,15 @@ import { createServer } from '~/src/server/index.js'
 import { statusCodes } from '~/src/server/common/constants/status-codes.js'
 import { parseDocument } from '~/src/server/common/test-helpers/dom.js'
 import { config } from '~/src/config/config.js'
-import { describePageSnapshot } from '../common/test-helpers/snapshot-page.js'
+import SessionTestHelper from '../common/test-helpers/session-helper.js'
+import { withCsrfProtection } from '../common/test-helpers/csrf.js'
 
 const pageUrl = '/'
 
 describe('#homeController', () => {
   /** @type {Server} */
   let server
+  let session
 
   beforeAll(async () => {
     server = await createServer()
@@ -17,6 +19,10 @@ describe('#homeController', () => {
 
   afterAll(async () => {
     await server.stop({ timeout: 0 })
+  })
+
+  beforeEach(async () => {
+    session = await SessionTestHelper.create(server)
   })
 
   it('Should provide expected response', async () => {
@@ -29,10 +35,71 @@ describe('#homeController', () => {
     expect(parseDocument(payload).title).toBe(config.get('serviceName'))
   })
 
-  describePageSnapshot({
-    describes: 'homeController.content',
-    it: 'should render expected response and content',
-    pageUrl
+  describe('v1', () => {
+    it('should match v1 content', async () => {
+      const featureFlags = {
+        ...config.get('featureFlags'),
+        biosecurity: false
+      }
+      jest.spyOn(config, 'get').mockImplementation((name) => {
+        if (name === 'featureFlags') {
+          return featureFlags
+        } else {
+          return 'Get permission to move animals under disease controls'
+        }
+      })
+
+      const { payload } = await server.inject(
+        withCsrfProtection(
+          {
+            method: 'GET',
+            url: pageUrl
+          },
+          {
+            Cookie: session.sessionID
+          }
+        )
+      )
+
+      const content =
+        parseDocument(payload).querySelector('#main-content')?.innerHTML
+
+      expect(content).toMatchSnapshot()
+    })
+  })
+
+  describe('v2', () => {
+    it('should match v2 content', async () => {
+      const featureFlags = {
+        ...config.get('featureFlags'),
+        biosecurity: true
+      }
+
+      jest.spyOn(config, 'get').mockImplementation((name) => {
+        if (name === 'featureFlags') {
+          return featureFlags
+        } else {
+          return 'Get permission to move animals under disease controls'
+        }
+      })
+
+      const { payload } = await server.inject(
+        withCsrfProtection(
+          {
+            method: 'GET',
+            url: pageUrl
+          },
+          {
+            Cookie: session.sessionID
+          }
+        )
+      )
+
+      const content =
+        parseDocument(payload).querySelector('#main-content')?.innerHTML
+
+      expect(content).toMatchSnapshot()
+    })
   })
 })
 
