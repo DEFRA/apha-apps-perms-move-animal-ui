022e87d64574e13ef8579bc12ba6bbe1035652f8
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-02-24 09:25:08 +0000

    desktop fix

--- user-journey-tests/wdio.browserstack.extended-desktop.conf.js
+++ user-journey-tests/wdio.browserstack.extended-desktop.conf.js
@@ -3,17 +3,17 @@ import { config as browserstackBaseConfig } from './wdio.browserstack.conf.js'
 //  These capabilities should run as an extended test after publishing
 export const config = {
   ...browserstackBaseConfig,
-  maxInstances: 4,
+  maxInstances: 8,
+  exclude: [
+    './specs/noJavascript/**/*.spec.js',
+    './specs/finalAnswers.spec.js',
+    './specs/submissionConfirmation.spec.js',
+    './specs/origin/checkAnswers.spec.js',
+    './specs/receiving-the-licence/licenceAnswers.spec.js',
+    './specs/biosecurity-map/biosecurityMapUpload.spec.js',
+    './specs/finalAnswersDeclarations.spec.js'
+  ],
   capabilities: [
-    {
-      browserName: 'Chrome',
-      'bstack:options': {
-        browserVersion: 'latest',
-        os: 'Windows',
-        osVersion: '10',
-        sessionName: 'windows-chrome'
-      }
-    },
     {
       browserName: 'Firefox',
       'bstack:options': {
@@ -24,21 +24,21 @@ export const config = {
       }
     },
     {
-      browserName: 'Chrome',
-      'bstack:options': {
-        browserVersion: 'latest',
-        os: 'OS X',
-        osVersion: 'Monterey',
-        sessionName: 'osx-chrome'
-      }
-    },
-    {
       browserName: 'Firefox',
       'bstack:options': {
         browserVersion: 'latest',
         os: 'OS X',
         osVersion: 'Monterey',
         sessionName: 'osx-firefox'
+      }
+    },
+    {
+      browserName: 'Safari',
+      'bstack:options': {
+        browserVersion: '15.6',
+        os: 'OS X',
+        osVersion: 'Monterey',
+        sessionName: 'osx-safari'
       }
     }
   ]
