b348e80cc074d0b765dc1b5cc030d2b8b4559034
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-02-25 17:13:58 +0000

    destination-farm-address

--- src/server/destination/destination-farm-address/index.js
+++ src/server/destination/destination-farm-address/index.js
@@ -1,7 +1,43 @@
-import { Page } from '../../common/model/page/page-model.js'
+import { QuestionPageController } from '../../common/controller/question-page-controller/question-page-controller.js'
+import { AddressAnswer } from '../../common/model/answer/address/address.js'
+import { QuestionPage } from '../../common/model/page/question-page-model.js'
+import { reasonForMovementPage } from '../reason-for-movement/index.js'
+import { destinationSummaryPage } from '../summary/index.js'
 
-export class DestinationFarmAddressPage extends Page {
+/**
+ * @import { AnswerModel } from '../../common/model/answer/answer-model.js'
+ * @import { ServerRegisterPluginObject } from '@hapi/hapi'
+ */
+
+export class DestinationFarmAddressPage extends QuestionPage {
   urlPath = '/destination/destination-farm-address'
+  sectionKey = 'destination'
+  question =
+    'What is the address of the farm or premises where the animals are going to?'
+
+  questionKey = 'destinationFarmAddress'
+
+  Answer = AddressAnswer
+
+  /**
+   * @param {AnswerModel} _answer
+   * @param {import('../../common/model/state/state-manager.js').RawApplicationState} context
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  nextPage(_answer, context) {
+    if (context.origin?.onOffFarm === 'on') {
+      return reasonForMovementPage
+    } else {
+      return destinationSummaryPage
+    }
+  }
 }
 
 export const destinationFarmAddressPage = new DestinationFarmAddressPage()
+
+/**
+ * @satisfies {ServerRegisterPluginObject<void>}
+ */
+export const destinationFarmAddress = new QuestionPageController(
+  destinationFarmAddressPage
+).plugin()
---
+++ src/server/destination/reason-for-movement/index.js
@@ -0,0 +1,7 @@
+import { Page } from '../../common/model/page/page-model.js'
+
+export class ReasonForMovementPage extends Page {
+  urlPath = '/destination/reason-for-movement'
+}
+
+export const reasonForMovementPage = new ReasonForMovementPage()
