b584ca608dfccc15bf592ec0add7cd0ef4f8aea1
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-02-26 10:44:26 +0000

    reduce cognitive complexity

--- src/server/check-answers/index.js
+++ src/server/check-answers/index.js
@@ -101,56 +101,12 @@ export class SubmitPageController extends QuestionPageController {
     const { isValid: isValidApplication } = application.validate()
 
     if (isValidPage && isValidApplication) {
-      const emailContent = Object.values(application.tasks)
-        .flatMap(({ questionPageAnswers }) =>
-          questionPageAnswers
-            .filter(({ page }) => !page.isInterstitial)
-            .map(
-              ({ page, answer }) =>
-                `## ${page.question}\n${answer.emailHtml.replace(/<br \/>/g, '\n')}`
-            )
-        )
-        .join('\n')
-
-      const notifyProps = {
-        content: emailContent
-      }
+      const emailContent = this.generateEmailContent(application)
+      const notifyProps = { content: emailContent }
 
       if (config.get('featureFlags').biosecurity) {
-        const obj = await req.s3.send(
-          new GetObjectCommand({
-            Bucket: config.get('fileUpload').bucket ?? '',
-            Key: req.yar.get('biosecurity-map')['upload-plan'].status.form.file
-              .s3Key
-          })
-        )
-
-        const chunks = []
-        for await (const chunk of obj.Body) {
-          chunks.push(chunk)
-        }
-        const buffer = Buffer.concat(chunks)
-
-        let compressedFile = null
-
-        const mimeOffset = 5
-        const isPdf = buffer.subarray(0, mimeOffset).toString() === '%PDF-'
-        if (isPdf && config.get('featureFlags').pdfUpload) {
-          const { duration, reduction, file } = await compressPDF(buffer)
-          compressedFile = file
-          this.logger.info(
-            `PDF compression took ${duration}ms at a reduction of ${reduction}% to ${fileSize(file.length)} MB`
-          )
-        } else {
-          const { duration, file, reduction } = await compressImage(buffer)
-          compressedFile = file
-          this.logger.info(
-            `Image compression took ${duration}ms at a reduction of ${reduction}% to ${fileSize(file.length)} MB`
-          )
-        }
-
-        if (fileSize(compressedFile.length) > 2) {
-          // code to handle file over 2MB
+        const compressedFile = await this.handleBiosecurityFile(req)
+        if (!compressedFile) {
           return Boom.badImplementation(
             'Compressed file is too large to send to Notify'
           )
@@ -158,10 +114,9 @@ export class SubmitPageController extends QuestionPageController {
 
         const { fileRetention, confirmDownloadConfirmation } =
           config.get('notify')
-
         notifyProps.link_to_file = {
           file: compressedFile?.toString('base64'),
-          filename: `Biosecurity-map.${isPdf ? 'pdf' : 'jpg'}`,
+          filename: `Biosecurity-map.${this.isPdf(compressedFile) ? 'pdf' : 'jpg'}`,
           confirm_email_before_download: confirmDownloadConfirmation,
           retention_period: fileRetention
         }
@@ -179,6 +134,64 @@ export class SubmitPageController extends QuestionPageController {
     }
 
     return super.handlePost(req, h)
+  }
+
+  generateEmailContent(application) {
+    return Object.values(application.tasks)
+      .flatMap(({ questionPageAnswers }) =>
+        questionPageAnswers
+          .filter(({ page }) => !page.isInterstitial)
+          .map(
+            ({ page, answer }) =>
+              `## ${page.question}\n${answer.emailHtml.replace(/<br \/>/g, '\n')}`
+          )
+      )
+      .join('\n')
+  }
+
+  async handleBiosecurityFile(req) {
+    const obj = await req.s3.send(
+      new GetObjectCommand({
+        Bucket: config.get('fileUpload').bucket ?? '',
+        Key: req.yar.get('biosecurity-map')['upload-plan'].status.form.file
+          .s3Key
+      })
+    )
+
+    const chunks = []
+    for await (const chunk of obj.Body) {
+      chunks.push(chunk)
+    }
+    const buffer = Buffer.concat(chunks)
+
+    let compressedFile = null
+
+    const mimeOffset = 5
+    const isPdf = buffer.subarray(0, mimeOffset).toString() === '%PDF-'
+    if (isPdf && config.get('featureFlags').pdfUpload) {
+      const { duration, reduction, file } = await compressPDF(buffer)
+      compressedFile = file
+      this.logger.info(
+        `PDF compression took ${duration}ms at a reduction of ${reduction}% to ${fileSize(file.length)} MB`
+      )
+    } else {
+      const { duration, file, reduction } = await compressImage(buffer)
+      compressedFile = file
+      this.logger.info(
+        `Image compression took ${duration}ms at a reduction of ${reduction}% to ${fileSize(file.length)} MB`
+      )
+    }
+
+    if (fileSize(compressedFile.length) > 2) {
+      return null
+    }
+
+    return compressedFile
+  }
+
+  isPdf(buffer) {
+    const mimeOffset = 5
+    return buffer.subarray(0, mimeOffset).toString() === '%PDF-'
   }
 }
 
