7ebb733e32a7cf21d2c726018da55b29b28d0270
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-02-26 11:35:53 +0000

    Reason for movement page

---
+++ src/server/destination/quantity-options/index.js
@@ -0,0 +1,7 @@
+import { Page } from '../../common/model/page/page-model.js'
+
+export class QuantityOptionsPage extends Page {
+  urlPath = '/destination/quantity-options'
+}
+
+export const quantityOptionsPage = new QuantityOptionsPage()
---
+++ src/server/destination/reason-for-movement/index.test.js
@@ -0,0 +1,67 @@
+import { reasonForMovementPage, ReasonForMovementPage } from './index.js'
+import { describePageSnapshot } from '../../common/test-helpers/snapshot-page.js'
+import { ReasonForMovementAnswer } from '../../common/model/answer/movement-reason/movement-reason.js'
+import { quantityOptionsPage } from '../quantity-options/index.js'
+import { additionalInfoPage } from '../additional-info/index.js'
+
+const sectionKey = 'destination'
+const question = 'What is the reason for the movement?'
+const questionKey = 'reasonForMovement'
+const view = 'common/model/page/question-page.njk'
+const pageUrl = '/destination/reason-for-movement'
+
+describe('ReasonForMovementPage', () => {
+  let page
+
+  beforeEach(() => {
+    page = new ReasonForMovementPage()
+  })
+
+  it('should have the correct urlPath', () => {
+    expect(page.urlPath).toBe(pageUrl)
+  })
+
+  it('should have the correct sectionKey', () => {
+    expect(page.sectionKey).toBe(sectionKey)
+  })
+
+  it('should have the correct question', () => {
+    expect(page.question).toBe(question)
+  })
+
+  it('should have the correct questionKey', () => {
+    expect(page.questionKey).toBe(questionKey)
+  })
+
+  it('should have the correct view', () => {
+    expect(page.view).toBe(view)
+  })
+
+  it('should have the correct Answer model', () => {
+    expect(page.Answer).toBe(ReasonForMovementAnswer)
+  })
+
+  it('nextPage should return quantity options page when answer is "routineRestocking"', () => {
+    const answer = new ReasonForMovementAnswer({
+      reasonForMovement: 'routineRestocking'
+    })
+    const nextPage = page.nextPage(answer)
+    expect(nextPage).toBe(quantityOptionsPage)
+  })
+
+  it('nextPage should return additional info page when answer is any other value', () => {
+    const answer = new ReasonForMovementAnswer({ reasonForMovement: 'other' })
+    const nextPage = page.nextPage(answer)
+    expect(nextPage).toBe(additionalInfoPage)
+  })
+
+  it('should export page', () => {
+    expect(reasonForMovementPage).toBeInstanceOf(ReasonForMovementPage)
+  })
+
+  describePageSnapshot({
+    describes: 'ReasonForMovementPage.content',
+    it: 'should render expected response and content',
+    pageUrl
+  })
+})
