18a521cdbaa38a614f042c3eaa25dc8be26586d3
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-27 13:09:31 +0000

    DSFAAP-879: wip - add basic logic

--- src/server/common/model/section/biosecurity/biosecurity.js
+++ src/server/common/model/section/biosecurity/biosecurity.js
@@ -2,8 +2,23 @@ import { keptSeparatelyPage } from '~/src/server/biosecurity/kept-separately/ind
 import { SectionModel } from '../section-model/section-model.js'
 import { config } from '~/src/config/config.js'
 import { biosecurity } from '~/src/server/biosecurity/index.js'
+import { OriginSection } from '../origin/origin.js'
+import { DestinationSection } from '../destination/destination.js'
 
+/** @import {RawApplicationState} from '~/src/server/common/model/state/state-manager.js' */
 /** @import {SectionConfig} from '../section-model/section-model.js' */
+
+/** @param {RawApplicationState} app */
+const isVisible = (app) => {
+  const isBiosecurityFlag = config.get('featureFlags')?.biosecurity
+  const isDestinationNotAfu = app.destination?.destinationType !== 'afu'
+  return (
+    isBiosecurityFlag &&
+    OriginSection.fromState(app).validate().isValid &&
+    DestinationSection.fromState(app).validate().isValid &&
+    isDestinationNotAfu
+  )
+}
 
 export class BiosecuritySection extends SectionModel {
   /** @type {SectionConfig} */
@@ -13,7 +28,7 @@ export class BiosecuritySection extends SectionModel {
     plugin: biosecurity,
     summaryLink: '/biosecurity/check-answers',
     isEnabled: () => true,
-    isVisible: () => config.get('featureFlags')?.biosecurity
+    isVisible
   }
 
   static firstPageFactory = () => keptSeparatelyPage
