e1c183f9b05c3493f2ab4bace2ae47a6c0c6c0cb
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-02-28 09:03:57 +0000

    DSFAAP-879: fix dynamic biosec test

--- user-journey-tests/specs/biosecurity/biosecDynamicAnswers.spec.js
+++ user-journey-tests/specs/biosecurity/biosecDynamicAnswers.spec.js
@@ -1,5 +1,7 @@
 import { selectElement, waitForPagePath } from '../../helpers/page.js'
 import completeBiosecurityTask from '../../helpers/testHelpers/biosecurity.js'
+import { completeDestinationTaskOnFarm } from '../../helpers/testHelpers/destination.js'
+import { completeOriginTaskAnswersOnFarm } from '../../helpers/testHelpers/movementLicence.js'
 import biosecurityAnswersPage from '../../page-objects/biosecurity/biosecurityAnswersPage.js'
 import grazingPage from '../../page-objects/biosecurity/grazingPage.js'
 import keptSeparatelyPage from '../../page-objects/biosecurity/keptSeparatelyPage.js'
@@ -7,6 +9,8 @@ import keptSeparatelyPage from '../../page-objects/biosecurity/keptSeparatelyPag
 describe('Check dynamic nature of flow - biosecurity', () => {
   // eslint-disable-next-line no-undef
   before('Complete answers', async () => {
+    await completeOriginTaskAnswersOnFarm()
+    await completeDestinationTaskOnFarm()
     await completeBiosecurityTask('no')
     await biosecurityAnswersPage.navigateToPageAndVerifyTitle()
   })
