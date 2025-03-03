97415be4943d391a12ac169c9a8ea4ce9b1b61cd
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-03-03 14:54:36 +0000

    BAU: add 'nextPage's where missing

--- user-journey-tests/helpers/testHelpers/movementLicence.js
+++ user-journey-tests/helpers/testHelpers/movementLicence.js
@@ -38,11 +38,14 @@ const completeOriginTask = async ({
       cphNumber,
       onFarmAddressPage
     )
-    await onFarmAddressPage.fillFormFieldsAndSubmit({
-      lineOne,
-      townOrCity,
-      postcode
-    })
+    await onFarmAddressPage.fillFormFieldsAndSubmit(
+      {
+        lineOne,
+        townOrCity,
+        postcode
+      },
+      fiftyPercentWarningPage
+    )
     await fiftyPercentWarningPage.selectContinue()
   } else {
     await toFromFarmPage.selectOffFarmAndContinue(originTypePage)
@@ -102,7 +105,7 @@ export const destinationVariants = async (onFarm, afu) => {
     else
       originTypePage.selectTBRestrictedFarmAndContinue(parishHoldingNumberPage)
   } else {
-    toFromFarmPage.selectOffFarmAndContinue()
+    toFromFarmPage.selectOffFarmAndContinue(originTypePage)
     if (afu)
       originTypePage.selectApprovedFinishingUnitAndContinue(onFarmCPHPage)
     else originTypePage.selectTBRestrictedFarmAndContinue(onFarmCPHPage)
