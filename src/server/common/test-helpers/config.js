5a63d5dd0410b33c96e2067777f9816934f91533
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-03-10 11:19:35 +0000

    DSFAAP-937: prevent recursion in spyOnConfig

--- src/server/common/test-helpers/config.js
+++ src/server/common/test-helpers/config.js
@@ -2,12 +2,13 @@ import { config } from '~/src/config/config.js'
 
 /* global jest */
 
+const configGet = config.get.bind(config)
+
 /**
  * @param {string} key
  * @param {any} value
  */
 export const spyOnConfig = (key, value) => {
-  const configGet = config.get.bind(config)
   jest.spyOn(config, 'get').mockImplementation((name) => {
     if (name === key) {
       return value
