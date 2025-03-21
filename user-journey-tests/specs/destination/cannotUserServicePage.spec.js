92c603dc43c092e7432efa19ebfbad523847bf5a
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-03-21 17:05:19 +0000

    Remove test not needed

--- user-journey-tests/specs/destination/cannotUserServicePage.spec.js
+++
@@ -1,8 +0,0 @@
-import canNotUseServicePage from '../../page-objects/destination/canNotUseServicePage.js'
-
-describe('Cannot use service', () => {
-  it('should display a page including a link to the TB204', async () => {
-    await canNotUseServicePage.navigateToPageAndVerifyTitle()
-    await canNotUseServicePage.verifyViewApplicationLink()
-  })
-})
