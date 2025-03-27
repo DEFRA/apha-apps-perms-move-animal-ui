ba096e99fb497fb92c03c427730b93e90500d829
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-03-27 10:14:45 +0000

    Add toString for pipeline run

--- user-journey-tests/page-objects/signInPage.js
+++ user-journey-tests/page-objects/signInPage.js
@@ -30,8 +30,8 @@ class SignInPage extends Page {
     await browser.url(taskListPage.pagePath)
     await this.verifyPageHeadingAndTitle()
 
-    await typeIntoElement(this.idInput, this.testId)
-    await typeIntoElement(this.passwordInput, this.testPassword)
+    await typeIntoElement(this.idInput.toString(), this.testId)
+    await typeIntoElement(this.passwordInput.toString(), this.testPassword)
 
     await waitForEnabled(this.signInButton, {
       timeoutMsg:
