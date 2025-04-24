37ba29bf00a0363f6269b9939d3d8431848c852f
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-04-24 15:23:07 +0100

    remove jest config change

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ export default {
   resetModules: true,
   clearMocks: true,
   silent: false,
-  testTimeout: 300000,
   testMatch: ['**/src/**/*.test.js', '**/user-journey-tests/config/*.test.js'],
   reporters: ['default', ['github-actions', { silent: false }], 'summary'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
