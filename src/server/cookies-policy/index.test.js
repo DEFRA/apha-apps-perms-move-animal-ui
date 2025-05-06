54e31bb9e4b9386e6bf45e37a3dfb03ab0e4843a
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-05-06 16:50:47 +0100

    updates to cookies page

--- src/server/cookies-policy/index.test.js
+++ src/server/cookies-policy/index.test.js
@@ -1,7 +1,33 @@
+import { spyOnConfig } from '../common/test-helpers/config.js'
 import { describePageSnapshot } from '../common/test-helpers/snapshot-page.js'
 
-describePageSnapshot({
-  describes: '#cookiesPolicyPage',
-  it: 'should render expected response and content',
-  pageUrl: '/cookies'
+describe('Cookies Policy Page', () => {
+  describe('when authentication is enabled', () => {
+    beforeEach(() => {
+      spyOnConfig('featureFlags', {
+        authEnabled: true,
+        authRequired: true
+      })
+    })
+
+    describePageSnapshot({
+      describes: '#cookiesPolicyPage',
+      it: 'should render expected response and content',
+      pageUrl: '/cookies'
+    })
+  })
+
+  describe('when authentication is disabled', () => {
+    beforeEach(() => {
+      spyOnConfig('featureFlags', {
+        authEnabled: false
+      })
+    })
+
+    describePageSnapshot({
+      describes: '#cookiesPolicyPage',
+      it: 'should render expected response and content',
+      pageUrl: '/cookies'
+    })
+  })
 })
