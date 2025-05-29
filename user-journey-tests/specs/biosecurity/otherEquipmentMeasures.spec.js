0b42859540533fcae70df90fc50756fa5ca66891
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-05-29 11:59:45 +0100

    New test

---
+++ user-journey-tests/specs/biosecurity/otherEquipmentMeasures.spec.js
@@ -0,0 +1,33 @@
+import signInPage from '../../page-objects/signInPage.js'
+import {
+  loginAndSaveSession,
+  restoreSession
+} from '../../helpers/authSessionManager.js'
+import otherEquipmentMeasuresPage from '../../page-objects/biosecurity/otherEquipmentMeasuresPage.js'
+import peopleDisinfectionPage from '../../page-objects/biosecurity/peopleDisinfectionPage.js'
+
+describe('Other equipment measures page spec', () => {
+  // eslint-disable-next-line no-undef
+  before(async () => {
+    await loginAndSaveSession(signInPage)
+  })
+
+  beforeEach('Restore session and navigate to page', async () => {
+    await restoreSession()
+    await otherEquipmentMeasuresPage.navigateToPageAndVerifyTitle()
+  })
+
+  it('Should verify that page errors when nothing is entered', async () => {
+    await otherEquipmentMeasuresPage.singleInputErrorTest(
+      '',
+      otherEquipmentMeasuresPage.noInputError
+    )
+  })
+
+  it('Should input correct input and continue without error', async () => {
+    await otherEquipmentMeasuresPage.inputTextAndContinue(
+      '1995',
+      peopleDisinfectionPage
+    )
+  })
+})
