6c5e9e40d318bb92f5301e512824ba986335d6a7
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-06-19 08:00:52 +0100

    updated test location

--- src/server/common/helpers/headers/index.test.js
+++ src/server/common/helpers/headers/index.test.js
@@ -12,26 +12,26 @@ const mockRequest = (/** @type {number} */ statusCode) => ({
   }
 })
 
-it.each(Array.from(Object.entries(headers)))(
-  'should set %s to expected value (%s)',
-  (header, headerValue) => {
-    const mockToolkit = {
-      view: mockToolkitView.mockReturnThis(),
-      code: mockToolkitCode.mockReturnThis()
-    }
-
-    // @ts-expect-error - Testing purposes only
-    allRequests(mockRequest(200), mockToolkit)
-
-    // Find the Permissions-Policy header argument
-    const headerCall = mockHeader.mock.calls.find(([name]) => name === header)
-    expect(headerCall).toBeDefined()
-
-    expect(headerValue).toContain(headerValue)
-  }
-)
-
 describe('#Headers', () => {
+  it.each(Array.from(Object.entries(headers)))(
+    'should set %s to expected value (%s)',
+    (header, headerValue) => {
+      const mockToolkit = {
+        view: mockToolkitView.mockReturnThis(),
+        code: mockToolkitCode.mockReturnThis()
+      }
+
+      // @ts-expect-error - Testing purposes only
+      allRequests(mockRequest(200), mockToolkit)
+
+      // Find the Permissions-Policy header argument
+      const headerCall = mockHeader.mock.calls.find(([name]) => name === header)
+      expect(headerCall).toBeDefined()
+
+      expect(headerValue).toContain(headerValue)
+    }
+  )
+
   it.each(features)(
     'should set Permissions-Policy header to disallow %s',
     (feature) => {
