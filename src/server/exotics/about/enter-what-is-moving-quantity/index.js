e21710a3db84138c34aad5fef07d04fea5795056
Hugh F D Jackson <hugh@hughfdjackson.com>
2025-07-01 15:05:13 +0100

    DSFAAP-1291: add textbox to inform us what you're moving

---
+++ src/server/exotics/about/enter-what-is-moving-quantity/index.js
@@ -0,0 +1,8 @@
+import { Page } from '~/src/server/common/model/page/page-model.js'
+
+export class EnterWhatIsMovingQuantityPage extends Page {
+  urlPath =
+    '/exotics/about-the-movement/what-is-moving/enter-what-is-moving/quantity'
+}
+
+export const enterWhatIsMovingQuantityPage = new EnterWhatIsMovingQuantityPage()
