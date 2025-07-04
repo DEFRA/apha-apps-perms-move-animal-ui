c95c7811d32a5fc977990ca874ad45cfb6c9d3a4
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-07-04 10:57:46 +0100

    update URL of stub page

--- src/server/exotics/about/type-of-bird-other/index.js
+++ src/server/exotics/about/type-of-bird-other/index.js
@@ -2,7 +2,7 @@ import { Page } from '~/src/server/common/model/page/page-model.js'
 
 export class TypeOfBirdOtherPage extends Page {
   urlPath =
-    '/exotics/about-the-movement/what-is-moving/select-animals/birds/other-birds'
+    '/exotics/about-the-movement/what-is-moving/select-animals/birds/enter-bird-type'
 }
 
 export const typeOfBirdOtherPage = new TypeOfBirdOtherPage()
