f761b1a5aa946afc00c1f5d72a3f69a6ad4b3a9d
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-07-08 11:05:46 +0100

    Exotics test 1

--- user-journey-tests/exotics/page-objects/about-the-movement/birdTypeInputPage.js
+++ user-journey-tests/exotics/page-objects/about-the-movement/birdTypeInputPage.js
@@ -5,6 +5,7 @@ const pageHeadingAndTitle = 'What type of bird are you moving?'
 class BirdTypeInputPage extends SingleInputBasePage {
   pagePath =
     'exotics/about-the-movement/what-is-moving/select-animals/birds/enter-bird-type'
+
   pageHeading = pageHeadingAndTitle
   pageTitle = pageHeadingAndTitle
 
