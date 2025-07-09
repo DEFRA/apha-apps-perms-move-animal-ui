f8e002181ea6228e46c41e0bbe5ae07df7fbd43f
Jose Garcia <joseluisgraa@gmail.com>
2025-07-09 10:12:17 +0100

    AnimalsOnPremisesPage

--- src/server/exotics/origin/animals-on-premises/index.js
+++ src/server/exotics/origin/animals-on-premises/index.js
@@ -1,8 +1,46 @@
-import { Page } from '~/src/server/common/model/page/page-model.js'
+import { QuestionPage } from '~/src/server/common/model/page/question-page-model.js'
+import { ExoticsQuestionPageController } from '~/src/server/exotics/question-page-controller.js'
+import { TextAreaAnswer } from '~/src/server/common/model/answer/text-area/text-area.js'
+import { checkAnswersPage } from '../check-answers/index.js'
 
-// STUB PAGE
-export class AnimalsOnPremisesPage extends Page {
+/** @import { TextAreaConfig } from '~/src/server/common/model/answer/text-area/text-area.js' */
+/** @import { ServerRegisterPluginObject } from '@hapi/hapi' */
+
+const questionKey = 'animalsOnPremises'
+
+export class Answer extends TextAreaAnswer {
+  /** @type { TextAreaConfig } */
+  static config = {
+    payloadKey: questionKey,
+    rows: 4,
+    spellcheck: false,
+    validation: {
+      maxLength: {
+        value: 5000,
+        message: 'Your answer must be no longer than 5000 characters'
+      },
+      empty: { message: 'Enter what animals are on the premises' }
+    }
+  }
+}
+
+export class AnimalsOnPremisesPage extends QuestionPage {
   urlPath = '/exotics/movement-origin/animals-onsite'
+
+  questionKey = questionKey
+  sectionKey = 'origin'
+  question = 'What animals are on the premises?'
+
+  Answer = Answer
+
+  nextPage() {
+    return checkAnswersPage
+  }
 }
 
 export const animalsOnPremisesPage = new AnimalsOnPremisesPage()
+
+/** @satisfies {ServerRegisterPluginObject<void>} */
+export const animalsOnPremises = new ExoticsQuestionPageController(
+  animalsOnPremisesPage
+).plugin()
