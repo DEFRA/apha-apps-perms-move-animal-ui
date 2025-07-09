ff4b564f18088b595fe1999613aebacc990fcb77
Hugh F D Jackson <hugh@hughfdjackson.com>
2025-07-09 08:49:10 +0100

    DSFAAP-1373: remove unused import

--- src/server/router.js
+++ src/server/router.js
@@ -16,7 +16,6 @@ import { authPlugin } from './auth/index.js'
 import { config } from '../config/config.js'
 import { ExoticsApplicationModel } from './exotics/application.js'
 import { exoticsTaskList } from './exotics/task-list/index.js'
-import { reason } from './exotics/visit-details/reason/index.js'
 
 /**
  * @satisfies {ServerRegisterPluginObject<void>}
