73ecf5bc5b473e344ed319e997817f6271340232
Jose Garcia <joseluisgraa@gmail.com>
2025-07-11 13:15:10 +0100

    initial version with original approach

--- src/server/exotics/destination/cph-needed/index.js
+++ src/server/exotics/destination/cph-needed/index.js
@@ -1,8 +1,28 @@
-import { Page } from '~/src/server/common/model/page/page-model.js'
+/** @import { ServerRegisterPluginObject } from '@hapi/hapi' */
 
-// STUB PAGE
-export class CphNeededPage extends Page {
+import { ExitPage } from '~/src/server/common/model/page/exit-page-model.js'
+import { ActionablePageController } from './actionable-exit-page-controller.js'
+
+export class CphNeededPage extends ExitPage {
   urlPath = '/exotics/movement-destination/cph-needed'
-}
 
+  pageTitle = `You need the destination's CPH number to continue your application`
+  view = `exotics/destination/cph-needed/index`
+  key = 'cphNeeded'
+  sectionKey = 'destination'
+
+  viewProps() {
+    return {
+      continueButtonText: 'Continue with CPH number',
+      continueButtonClasses: 'govuk-button--secondary'
+    }
+  }
+}
 export const cphNeededPage = new CphNeededPage()
+
+/**
+ * @satisfies {ServerRegisterPluginObject<void>}
+ */
+export const cphNeeded = new ActionablePageController(cphNeededPage, {
+  methods: ['GET', 'POST']
+}).plugin()
