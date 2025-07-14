4e68823512745fd45044c60b475394a3b0fb660c
Hugh F D Jackson <hugh@hughfdjackson.com>
2025-07-14 09:03:25 +0100

    DSFAAP-1427: add prototype mode banner

--- src/server/exotics/task-list/index.test.js
+++ src/server/exotics/task-list/index.test.js
@@ -2,6 +2,12 @@ import { describePageSnapshot } from '../../common/test-helpers/snapshot-page.js
 import { ExoticsApplicationModel } from '../application.js'
 import { ExoticsStateManager } from '../state-manager.js'
 import { ExoticsTaskListController } from './index.js'
+import { createServer } from '../../index.js'
+import { withCsrfProtection } from '../../common/test-helpers/csrf.js'
+import { parseDocument } from '../../common/test-helpers/dom.js'
+import { spyOnConfig } from '../../common/test-helpers/config.js'
+
+/** @import { Server } from '@hapi/hapi' */
 
 describe('ExoticsTaskListController', () => {
   const controller = new ExoticsTaskListController()
@@ -32,5 +38,42 @@ describe('ExoticsTaskListController', () => {
     describes: 'ExoticsTaskListController',
     it: 'should have the expected content',
     pageUrl: controller.urlPath
+  })
+
+  describe('task list banner', () => {
+    /** @type {Server} */
+    let server
+
+    beforeAll(async () => {
+      server = await createServer()
+      await server.initialize()
+    })
+
+    afterAll(async () => {
+      await server.stop({ timeout: 0 })
+    })
+
+    it('should have the right banner (in protype mode)', async () => {
+      spyOnConfig('featureFlags', {
+        prototypeMode: true
+      })
+
+      const { payload } = await server.inject(
+        withCsrfProtection({
+          method: 'GET',
+          url: controller.urlPath
+        })
+      )
+
+      const contentTag = parseDocument(payload)
+        .querySelector('.govuk-phase-banner__content__tag')
+        ?.innerHTML.trim()
+      const text = parseDocument(payload)
+        .querySelector('.govuk-phase-banner__text')
+        ?.innerHTML.trim()
+
+      expect(contentTag).toBe('Prototype')
+      expect(text).toBe('This is a service prototype.')
+    })
   })
 })
