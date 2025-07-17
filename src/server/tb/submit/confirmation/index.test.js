94613f72a71ab71631db765ea33f9af2c2eeee1a
Jose Garcia <joseluisgraa@gmail.com>
2025-07-17 12:39:58 +0100

    updates to confirmation page

---
+++ src/server/tb/submit/confirmation/index.test.js
@@ -0,0 +1,30 @@
+import { describePageSnapshot } from '~/src/server/common/test-helpers/snapshot-page.js'
+import { tbConfirmationPage } from './index.js'
+
+const pageTitle = 'Your animal disease movement licence application'
+
+describe('TbConfirmationPage', () => {
+  const page = tbConfirmationPage
+
+  it('should have the right key', () => {
+    expect(page.key).toBe('tb-submit-confirmation')
+  })
+
+  it('should have the right url', () => {
+    expect(page.urlPath).toBe('/tb/submit/confirmation')
+  })
+
+  it('should have the right page title', () => {
+    expect(page.pageTitle).toBe(pageTitle)
+  })
+
+  it('should have the right heading', () => {
+    expect(page.heading).toBe(pageTitle)
+  })
+
+  describePageSnapshot({
+    describes: 'TbConfirmationPage',
+    it: 'should have the expected content',
+    pageUrl: page.urlPath
+  })
+})
