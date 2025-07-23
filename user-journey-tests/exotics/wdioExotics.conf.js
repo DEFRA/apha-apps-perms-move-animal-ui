d2709ead3ac81cfc6adb1524c1c1a761ea79e65f
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-07-23 19:04:48 +0100

    update

--- user-journey-tests/exotics/wdioExotics.conf.js
+++ user-journey-tests/exotics/wdioExotics.conf.js
@@ -2,7 +2,7 @@ import merge from 'deepmerge'
 import { config as wdioConf } from '../wdio.conf.js'
 
 export const config = merge(wdioConf, {
-  specs: ['./specs/location-of-visit/visitLocationDoms.spec.js'],
+  specs: ['./specs/**/*.spec.js'],
   exclude: [],
   maxInstances: 7,
   capabilities: [
@@ -10,7 +10,7 @@ export const config = merge(wdioConf, {
       browserName: 'chrome',
       browserVersion: 'stable',
       'goog:chromeOptions': {
-        args: ['disable-gpu']
+        args: ['headless', 'disable-gpu']
       }
     }
   ]
