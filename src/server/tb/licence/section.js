df61a53e75719289ebe91bc712e0dcff415e99d4
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-07-28 08:28:23 +0100

    updated isENabled logic

--- src/server/tb/licence/section.js
+++ src/server/tb/licence/section.js
@@ -2,6 +2,7 @@ import { licence } from '~/src/server/tb/licence/index.js'
 import { SectionModel } from '~/src/server/common/model/section/section-model/section-model.js'
 import { fullNamePage } from '~/src/server/tb/licence/full-name/index.js'
 import { fullNameFuturePage } from '~/src/server/tb/licence/full-name-future/index.js'
+import { OriginSection } from '../origin/section.js'
 
 /** @import {SectionConfig} from '~/src/server/common/model/section/section-model/section-model.js' */
 
@@ -12,7 +13,7 @@ export class LicenceSection extends SectionModel {
     title: 'Receiving the licence',
     plugin: licence,
     summaryLink: '/receiving-the-licence/check-answers',
-    isEnabled: () => true,
+    isEnabled: (app) => OriginSection.fromState(app).validate().isValid,
     isVisible: () => true
   }
 
