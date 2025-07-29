a64133b5888ab81bb067b2b3fb793d10bf96d867
Jose Garcia <joseluisgraa@gmail.com>
2025-07-29 17:21:54 +0100

    empty commit

--- src/server/tb/licence/check-answers/index.test.js
+++ src/server/tb/licence/check-answers/index.test.js
@@ -5,7 +5,6 @@ const pageUrl = '/receiving-the-licence/check-answers'
 const testName = 'test_name'
 const testSurname = 'test_surname'
 const testEmail = 'test@email.com'
-
 const defaultState = {
   fullName: {
     firstName: testName,
