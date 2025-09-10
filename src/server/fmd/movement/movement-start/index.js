aa8e76cd17dbdae574fda114e9017d2c41a236a4
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-09-10 09:25:27 +0100

    updated page heading

--- src/server/fmd/movement/movement-start/index.js
+++ src/server/fmd/movement/movement-start/index.js
@@ -54,6 +54,12 @@ export class MovementStartPage extends QuestionPage {
 
   Answer = Answer
 
+  viewProps() {
+    return Promise.resolve({
+      heading: 'Movement dates'
+    })
+  }
+
   nextPage() {
     return movementEndPage
   }
