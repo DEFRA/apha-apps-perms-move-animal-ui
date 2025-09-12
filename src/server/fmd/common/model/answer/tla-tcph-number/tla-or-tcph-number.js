3a6856f6420d5a533301c528971fdc31adb01410
Jose Garcia <joseluisgraa@gmail.com>
2025-09-12 15:05:24 +0100

    extending cphNumber instead of textAnswer

--- src/server/fmd/common/model/answer/tla-tcph-number/tla-or-tcph-number.js
+++ src/server/fmd/common/model/answer/tla-tcph-number/tla-or-tcph-number.js
@@ -2,24 +2,17 @@ import {
   CphNumberAnswer,
   cphNumberRegex
 } from '~/src/server/common/model/answer/cph-number/cph-number.js'
-import { TextAnswer } from '~/src/server/common/model/answer/text/text.js'
-/** @import {TextConfig} from '~/src/server/common/model/answer/text/text.js' */
 
-/**
- * @typedef {{ tlaOrTcphNumber: string }} TlaOrTcphNumberPayload
- */
+/** @import {TextConfig} from '~/src/server/common/model/answer/text/text.js' */
 
 const invalidTlaOrTcphNumberError =
   'Enter the TLA or tCPH number in the correct format, for example, 12/345/6789'
 const emptyTlaOrTcphNumberError = 'Enter the TLA or tCPH number'
 
-/**
- * @augments {TextAnswer<TlaOrTcphNumberPayload>}
- */
-export class TlaOrTcphNumberAnswer extends TextAnswer {
+export class TlaOrTcphNumberAnswer extends CphNumberAnswer {
   /** @type {TextConfig} */
   static config = {
-    ...CphNumberAnswer.config,
+    ...super.config,
     payloadKey: 'tlaOrTcphNumber',
     validation: {
       maxLength: { value: 11, message: invalidTlaOrTcphNumberError },
--- src/server/fmd/common/model/answer/tla-tcph-number/tla-or-tcph-number.test.js
+++ src/server/fmd/common/model/answer/tla-tcph-number/tla-or-tcph-number.test.js
@@ -1,16 +1,16 @@
 import { TextAnswer } from '~/src/server/common/model/answer/text/text.js'
-import { TlaOrTcphNumberAnswer } from './tla-tcph-number.js'
+import { TlaOrTcphNumberAnswer } from './tla-or-tcph-number.js'
 import { cphNumberRegex } from '~/src/server/common/model/answer/cph-number/cph-number.js'
 
-/** @import {TlaOrTcphNumberPayload} from './tla-tcph-number.js' */
+/** @import {CphNumberPayload} from '~/src/server/common/model/answer/cph-number/cph-number.js' */
 
 const maxLength = 11
 const invalidTlaOrTcphNumberError =
   'Enter the TLA or tCPH number in the correct format, for example, 12/345/6789'
 
-/** @type {TlaOrTcphNumberPayload} */
+/** @type {CphNumberPayload} */
 const payload = {
-  tlaOrTcphNumber: '11/222/3333'
+  cphNumber: '11/222/3333'
 }
 
 describe('TlaOrTcphNumberAnswer', () => {
