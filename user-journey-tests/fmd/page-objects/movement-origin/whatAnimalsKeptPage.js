591522358d9a3d0917849c3767aefb0584050a85
Jose Luis Garcia <joseluisgraa@gmail.com>
2025-09-15 17:14:28 +0100

    fixing some lint issues

--- user-journey-tests/fmd/page-objects/movement-origin/whatAnimalsKeptPage.js
+++ user-journey-tests/fmd/page-objects/movement-origin/whatAnimalsKeptPage.js
@@ -5,13 +5,13 @@ const pageHeadingAndTitle = 'What animals are kept on the premises?'
 const noInputError = 'Select what animals are kept on the premises'
 
 const checkboxIds = [
-  'whatAnimals', //cattle
-  'whatAnimals-2', //sheep
-  'whatAnimals-3', //goats
-  'whatAnimals-4', //pigs
-  'whatAnimals-5', //camelids
-  'whatAnimals-6', //other-hooved
-  'whatAnimals-8' //none
+  'whatAnimals', // cattle
+  'whatAnimals-2', // sheep
+  'whatAnimals-3', // goats
+  'whatAnimals-4', // pigs
+  'whatAnimals-5', // camelids
+  'whatAnimals-6', // other-hooved
+  'whatAnimals-8' // none
 ]
 
 class WhatAnimalsKeptPage extends CheckboxBasePage {
