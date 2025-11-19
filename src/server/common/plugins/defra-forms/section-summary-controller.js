05b408d6ad32e1d9710021363d055c88cfeb2313
Hugh F D Jackson <hugh@hughfdjackson.com>
2025-11-19 22:01:53 +0000

    DSFAAP-2047: fix title & undo unnecessary inheritance

--- src/server/common/plugins/defra-forms/section-summary-controller.js
+++ src/server/common/plugins/defra-forms/section-summary-controller.js
@@ -1,11 +1,11 @@
 import { proceed } from '@defra/forms-engine-plugin/engine/helpers.js'
 import { FormAction } from '@defra/forms-engine-plugin/types'
 import { mapFormContextToAnswers } from './form-context.js'
-import { SummaryPageController } from '@defra/forms-engine-plugin/controllers/index.js'
+import { QuestionPageController } from '@defra/forms-engine-plugin/controllers/index.js'
 
 /** @import { SummaryListRow } from '@defra/forms-engine-plugin/types' */
 
-export class SectionSummaryPageController extends SummaryPageController {
+export class SectionSummaryPageController extends QuestionPageController {
   isSummaryPage = true
   viewName = 'defra-forms/check-answers'
   continueDestination = '/'
