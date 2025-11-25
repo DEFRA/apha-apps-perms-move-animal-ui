80eafe7687d2577a3f183eecd0da421f52fc0672
Mukesh Rawat <mukeshrawat02@gmail.com>
2025-11-25 21:09:11 +0000

    Refactored licence task completion to include your name page for TB restricted flows

--- user-journey-tests/TB/helpers/testHelpers/receivingLicence.js
+++ user-journey-tests/TB/helpers/testHelpers/receivingLicence.js
@@ -1,6 +1,7 @@
 import emailPage from '../../page-objects/receiving-the-licence/emailPage.js'
 import licenceAnswersPage from '../../page-objects/receiving-the-licence/licenceAnswersPage.js'
 import ownerNamePage from '../../page-objects/receiving-the-licence/ownerNamePage.js'
+import yourNamePage from '../../page-objects/receiving-the-licence/yourNamePage.js'
 import taskListPage from '../../page-objects/taskListPage.js'
 
 import { validateElementVisibleAndText } from '../page.js'
@@ -10,17 +11,33 @@ import { navigateToTaskList } from './taskListNav.js'
 const defaultEmail = 'eoin.corr@esynergy.co.uk'
 const defaultFirstName = 'Bruce'
 const defaultLastName = 'Wayne'
+const defaultYourFirstName = 'Clark'
+const defaultYourLastName = 'Kent'
 
 // Helper function to complete the origin task
 const completeLicenceTask = async ({
   email = defaultEmail,
   firstName = defaultFirstName,
   lastName = defaultLastName,
+  yourFirstName = defaultYourFirstName,
+  yourLastName = defaultYourLastName,
   on = false
 } = {}) => {
   await navigateToTaskList()
   await taskListPage.selectReceiveTheLicence(ownerNamePage)
-  await ownerNamePage.inputTextAndContinue(firstName, lastName, emailPage)
+
+  // When on-farm with TB restricted origin and destination, the flow includes yourNamePage
+  if (on) {
+    await ownerNamePage.inputTextAndContinue(firstName, lastName, yourNamePage)
+    await yourNamePage.inputTextAndContinue(
+      yourFirstName,
+      yourLastName,
+      emailPage
+    )
+  } else {
+    await ownerNamePage.inputTextAndContinue(firstName, lastName, emailPage)
+  }
+
   await emailPage.inputTextAndContinue(email, licenceAnswersPage)
 
   await licenceAnswersPage.verifyPageHeadingAndTitle()
