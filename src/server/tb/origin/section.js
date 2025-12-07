82b9faff0bf3c27de2319e86f925ce608c7ad7e6
Jose Garcia <joseluisgraa@gmail.com>
2025-12-07 11:37:07 +0000

    create section using v2 when toggle is on

--- src/server/tb/origin/section.js
+++ src/server/tb/origin/section.js
@@ -1,19 +1,46 @@
 import { SectionModelV1 } from '~/src/server/common/model/section/section-model/section-model-v1.js'
+import { SectionModelV2 } from '~/src/server/common/model/section/section-model/section-model-v2.js'
 import { onOffFarmPage } from './on-off-farm/index.js'
 import { origin } from '~/src/server/tb/origin/index.js'
+import { config } from '~/src/config/config.js'
 
-/** @import {SectionConfig} from '~/src/server/common/model/section/section-model/section-model.js' */
+/** 
+ * @import {SectionConfig} from '~/src/server/common/model/section/section-model/section-model.js' 
+ * @import {Request} from '@hapi/hapi'
+ * @import {RawApplicationState} from '~/src/server/common/model/state/state-manager.js'
+ */
 
-export class OriginSection extends SectionModelV1 {
+// Toggle between V1 and V2 (Defra Forms) implementations
+const defraFormsEnabled = config.get('featureFlags')?.defraFormsEnabled
+
+/** @type {typeof SectionModelV1 | typeof SectionModelV2} */
+const EnabledSectionModel = defraFormsEnabled ? SectionModelV2 : SectionModelV1
+
+export class OriginSection extends EnabledSectionModel {
+  /**
+   * @param {Request} req
+   * @param {RawApplicationState} state
+   * @returns {Promise<OriginSection>}
+   */
+  static async fromRequest(req, state) {
+    // @ts-ignore - TypeScript can't infer that both parent classes have fromRequest
+    return super.fromRequest(req, state)
+  }
   /** @type {SectionConfig} */
   static config = {
     key: 'origin',
     title: 'Movement origin',
     plugin: origin,
-    summaryLink: '/origin/check-answers',
+    summaryLink: defraFormsEnabled
+      ? '/tb-origin/summary'
+      : '/origin/check-answers',
     isEnabled: () => true,
     isVisible: () => true
   }
 
+  // V1 specific
   static firstPageFactory = () => onOffFarmPage
+
+  // V2 specific
+  static journeySlug = 'tb-origin'
 }
