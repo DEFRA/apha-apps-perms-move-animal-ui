2f19243fbc0424fc5afb510c4ae6069682386560
Jose Garcia <joseluisgraa@gmail.com>
2026-05-26 15:33:20 +0100

    Updates to visibility rules for biosecurity sections

--- src/server/common/model/section/visibility.js
+++ src/server/common/model/section/visibility.js
@@ -1,5 +1,7 @@
 import { OriginSection } from '../../../tb/origin/section.js'
 import { DestinationSection } from '../../../tb/destination/section.js'
+import { OriginTypeAnswer } from '../answer/origin-type/origin-type.js'
+import { DestinationTypeAnswer } from '../answer/destination-type/destination-type.js'
 
 /**
  * @import {Request} from '@hapi/hapi'
@@ -19,17 +21,30 @@ export const biosecuritySectionIsVisible = async (app, req) => {
   const originData = origin.sectionData.questionAnswers
   const destinationData = destination.sectionData.questionAnswers
 
-  const isMovementOn = originData.some(
-    (q) => q.questionKey === 'onOffFarm' && q.answer.value === 'on'
-  )
-  const isDestinationNotAfu = destinationData.some(
-    (q) => q.questionKey === 'destinationType' && q.answer.value !== 'afu'
+  const movementOnOff = originData.find((q) => q.questionKey === 'onOffFarm')
+    ?.answer.value
+  const isMovementOn = movementOnOff === 'on'
+  const isMovementOff = movementOnOff === 'off'
+
+  const originType = originData.find((q) => q.questionKey === 'originType')
+    ?.answer.value
+  const destinationType = destinationData.find(
+    (q) => q.questionKey === 'destinationType'
+  )?.answer.value
+  const isDestinationNotAfu = destinationType !== 'afu'
+  const bothTbRestricted =
+    OriginTypeAnswer.isTbRestricted(originType) &&
+    DestinationTypeAnswer.isTbRestricted(destinationType)
+  const ownBothOriginAndDestination = destinationData.some(
+    (q) =>
+      q.questionKey === 'ownBothOriginAndDestination' &&
+      q.answer.value === 'yes'
   )
 
   return (
-    isMovementOn &&
     origin.validate().isValid &&
     destination.validate().isValid &&
-    isDestinationNotAfu
+    ((isMovementOn && isDestinationNotAfu) ||
+      (isMovementOff && bothTbRestricted && ownBothOriginAndDestination))
   )
 }
